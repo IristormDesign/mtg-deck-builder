1f0097dc66d09e51fb11e3de6dbec56ba93932e0
Damian Schmidt <damian@iristormdesign.com>
2020-09-02 19:27:49 -0400

    Misc

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="site-footer">
-		<p>Web design and development by <a href="https://iristormdesign.com">Iristorm Design</a>. Copyright &copy; 2020 Damian Schmidt.</p>
-		<p><i>Magic: The Gathering</i> material is TM and &copy; by Wizards of the Coast. Neither this app nor Iristorm Design are affiliated with Wizards of the Coast.</p>
+		<p>Website design and development by <a href="https://iristormdesign.com">Iristorm Design</a>. Copyright &copy; 2020 Damian Schmidt.</p>
+		<p><i>Magic: The Gathering</i> material is TM and &copy; by <a href="https://wizards.com/" target="_blank" rel="noopener">Wizards of the Coast</a>. This web app is not affiliated with Wizards of the Coast.</p>
 	</footer>
 </template>
 
