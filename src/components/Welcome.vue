35e3bb1db7174f3f6e0c7e5d98686bd0a4cd3907
Damian Schmidt <damian@iristormdesign.com>
2020-12-15 12:49:03 -0500

    M

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -11,7 +11,7 @@
 			</div>
 		</div>
 		<footer>
-			<small>Illustration: “Lezard” by <a href="https://www.davidrevoy.com/article24/video-timelapse-lezard" target="_blank" rel="noopener">David Revoy</a>, used with <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noopener" title="Creative Commons Attribution 3.0 Unported">CC BY 3.0 license</a></small>
+			<small>ILLUSTRATION: “Lezard” by <a href="https://www.davidrevoy.com/article24/video-timelapse-lezard" target="_blank" rel="noopener">David Revoy</a>. Used with <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noopener" title="Creative Commons Attribution 3.0 Unported">CC BY 3.0 license</a>.</small>
 		</footer>
 	</article>
 </template>
