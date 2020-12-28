5820a227dbccb6b00d49e1b5cd3788a8f6c08579
Damian Schmidt <damian@iristormdesign.com>
2020-12-28 10:43:30 -0500

    Minor code optimization

--- src/components/DeckDateEdited.vue
+++ src/components/DeckDateEdited.vue
@@ -3,8 +3,8 @@
 		<h3>Last Edited On</h3>
 
 		<div class="date">
-			{{ dateEdited().date }}<br>
-			{{ dateEdited().time }}
+			{{ dateEdited }}<br>
+			{{ timeEdited }}
 		</div>
 	</div>
 </template>
@@ -15,84 +15,79 @@ export default {
 	props: {
 		deck: Object
 	},
-	methods: {
-		dateEdited () {
-			const date = this.deck.editDate
+	computed: {
+		dateEdited: function () {
+			const dateAndTime = this.deck.editDate
+			let month = dateAndTime.getMonth() + 1
+			const day = dateAndTime.getDate()
+			const year = dateAndTime.getFullYear()
 
-			if (date instanceof Date) {
-				let month = date.getMonth() + 1
-				const day = date.getDate()
-				const year = date.getFullYear()
-				let hour = date.getHours()
-				let minute = date.getMinutes()
-				let second = date.getSeconds()
-				let meridiem
-
-				switch (month) {
-				case 1:
-					month = 'Jan.'
-					break
-				case 2:
-					month = 'Feb.'
-					break
-				case 3:
-					month = 'Mar.'
-					break
-				case 4:
-					month = 'Apr.'
-					break
-				case 5:
-					month = 'May'
-					break
-				case 6:
-					month = 'June'
-					break
-				case 7:
-					month = 'July'
-					break
-				case 8:
-					month = 'Aug.'
-					break
-				case 9:
-					month = 'Sep.'
-					break
-				case 10:
-					month = 'Oct.'
-					break
-				case 11:
-					month = 'Nov.'
-					break
-				case 12:
-					month = 'Dec.'
-					break
-				}
-
-				if (hour < 12) {
-					meridiem = 'a.m.'
-				} else {
-					hour = hour - 12
-					meridiem = 'p.m.'
-				}
-
-				if (hour < 10) {
-					hour = '0' + hour
-				}
-				if (minute < 10) {
-					minute = '0' + minute
-				}
-				if (second < 10) {
-					second = '0' + second
-				}
-
-				const output = {
-					date: `${month} ${day}, ${year}`,
-					time: `${hour}:${minute}:${second} ${meridiem}`
-				}
-
-				return output
-			} else {
-				return false
+			switch (month) {
+			case 1:
+				month = 'Jan.'
+				break
+			case 2:
+				month = 'Feb.'
+				break
+			case 3:
+				month = 'Mar.'
+				break
+			case 4:
+				month = 'Apr.'
+				break
+			case 5:
+				month = 'May'
+				break
+			case 6:
+				month = 'June'
+				break
+			case 7:
+				month = 'July'
+				break
+			case 8:
+				month = 'Aug.'
+				break
+			case 9:
+				month = 'Sep.'
+				break
+			case 10:
+				month = 'Oct.'
+				break
+			case 11:
+				month = 'Nov.'
+				break
+			case 12:
+				month = 'Dec.'
+				break
 			}
+
+			return `${month} ${day}, ${year}`
+		},
+		timeEdited: function () {
+			const dateAndTime = this.deck.editDate
+			let hour = dateAndTime.getHours()
+			let minute = dateAndTime.getMinutes()
+			let second = dateAndTime.getSeconds()
+			let meridiem
+
+			if (hour < 12) {
+				meridiem = 'a.m.'
+			} else {
+				hour = hour - 12
+				meridiem = 'p.m.'
+			}
+
+			if (hour < 10) {
+				hour = '0' + hour
+			}
+			if (minute < 10) {
+				minute = '0' + minute
+			}
+			if (second < 10) {
+				second = '0' + second
+			}
+
+			return `${hour}:${minute}:${second} ${meridiem}`
 		}
 	}
 }
