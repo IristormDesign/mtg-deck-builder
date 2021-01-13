ab6799ecff9b013bd7bb113e6b385be226f270cb
Damian Schmidt <damian@iristormdesign.com>
2021-01-12 22:14:04 -0500

    Fixed bug with adding double-faced cards

--- src/components/DeckCardAdder.vue
+++ src/components/DeckCardAdder.vue
@@ -72,15 +72,44 @@ export default {
 					.then(response => {
 						const rd = response.data
 						const newCard = {
-							name: rd.name,
-							type: rd.type_line,
-							mana: rd.mana_cost,
-							cmc: rd.cmc,
-							colors: rd.colors,
-							img: rd.image_uris.normal,
 							link: rd.scryfall_uri,
 							qty: 1
 						}
+
+						if (rd.card_faces) { // If the card is a double-faced or split card...
+							const rdFace1 = rd.card_faces[0]
+							const rdFace2 = rd.card_faces[1]
+
+							newCard.name = `${rdFace1.name} / ${rdFace2.name}`
+
+							newCard.mana = `${rdFace1.mana_cost}`
+							if (rdFace2.mana_cost !== '') { // If the card's second face has its own mana cost, add it onto the mana cost string with a slash before it.
+								newCard.mana += '/' + rdFace2.mana_cost
+							}
+
+							newCard.type = `${rdFace1.type_line} / ${rdFace2.type_line}`
+							newCard.cmc = rd.cmc
+
+							if (rd.colors) {
+								newCard.colors = rd.colors
+							} else {
+								newCard.colors = rdFace1.colors
+							}
+
+							if (rd.image_uris) {
+								newCard.img = rd.image_uris.normal
+							} else {
+								newCard.img = rdFace1.image_uris.normal
+							}
+						} else { // Else the card is a single-faced card.
+							newCard.name = rd.name
+							newCard.mana = rd.mana_cost
+							newCard.type = rd.type_line
+							newCard.cmc = rd.cmc
+							newCard.colors = rd.colors
+							newCard.img = rd.image_uris.normal
+						}
+
 						if (newCard.colors.length >= 2) {
 							newCard.colors.unshift('multicolor')
 						}
