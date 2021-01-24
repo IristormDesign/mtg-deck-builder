571a7f7c673a2e018bc68da21bc978e767d0d69e
Damian Schmidt <damian@iristormdesign.com>
2021-01-24 14:25:06 -0500

    M

--- src/components/DeckCardDisplay.vue
+++ src/components/DeckCardDisplay.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="card-display">
 		<div v-for="card in deck.cards" :key="card.name">
-				<a
-					:href="card.link" target="_blank"
-					title="Click to view this card’s page on Scryfall"
-				>
+			<a
+				:href="card.link" target="_blank"
+				title="Click to view this card’s page on Scryfall"
+			>
 				<transition name="card-browse">
 					<img
 						v-show="deck.viewedCard == card.name"
