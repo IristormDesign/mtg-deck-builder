a2b629aaa32e5c76e93c91c2f3864d9132901a76
Damian Schmidt <damian@iristormdesign.com>
2021-03-13 17:53:37 -0500

    M

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -3,7 +3,7 @@
 		<div class="wrap">
 			<p><strong>MTG Deck Organizer</strong> — Version 1.0.0</p>
 			<p>Web design and development by Damian Schmidt of <a href="https://iristormdesign.com">Iristorm Design</a>.</p>
-			<p><i>Magic: The Gathering</i> content is TM and &copy; by Wizards of the Coast. This web app is not affiliated with or  endorsed by Wizards of the Coast, but is allowed under their Fan Content Policy. <br>The Scryfall API provides the data and images of cards. This web app is not affiliated with or endorsed by Scryfall.</p>
+			<p><i>Magic: The Gathering</i> content is TM and &copy; by Wizards of the Coast. This web app is not affiliated with or  endorsed by Wizards of the Coast, but is allowed under their Fan Content Policy. <br>The data and images of cards are provided by the Scryfall API. This web app is not affiliated with or endorsed by Scryfall.</p>
 			<div class="app-options">
 				<button class="app-reset" @click="clearLocalStorage()">Reset</button>
 			</div>
