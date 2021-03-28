8ec2bb7f716140ecc9a9fa0e52d452657e9d6bc8
Damian Schmidt <damian@iristormdesign.com>
2021-03-28 11:25:50 -0400

    Edits to legal docs

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -2,18 +2,21 @@
 	<article class="solo-message">
 		<h2>Privacy Policy</h2>
 
-		<p><i>Last updated on March 27, 2021</i></p>
-		<p>Iristorm Design (“we,” “us,” “our”) operates this website, MTG Deck Organizer (“MDO”). This document explains how we collect and handle personal information received from users of MDO. By continuing to use MDO, you agree to this privacy policy.</p>
+		<p><i>Last revised on March 27, 2021</i></p>
+
+		<p>Iristorm Design (“we,” “us,” “our”) operates this website, MTG Deck Organizer (“MDO”). This document explains how we collect and handle personal information received from users of MDO.</p>
+		<p>By using MDO, you agree to this privacy policy and the <router-link to="/terms">terms of service</router-link>.</p>
 
 		<h3>Stored Browser Data</h3>
 		<p>MDO uses web browser cookies and local web storage for storing personalized user data. Local storage is required in order for MDO to fully function.</p>
 		<p>To delete at once all of your customized MDO data in your browser’s local storage, click the “Clear Stored Data” link located in the footer of this site.</p>
 
 		<h3>Statistical Data</h3>
-		<p>We use Google Analytics to collect and analyze aggregated statistical data sent by people’s web browsers whenever they visit MDO. This data helps us better decide on how to improve the website.</p>
+		<p>We use Google Analytics to collect and analyze anonymous statistical data sent by people’s web browsers whenever they visit MDO. This data helps us better decide on how to improve the website.</p>
 		<p>The data includes technical information such as the web browser name and version, operating system name and version, device screen resolution, and country/state of access. It also includes behavioral information such as the pages on MDO that people visit, the dates of their visits, and the time spent on pages.</p>
 
-		<h3>Changes to This Privacy Policy</h3>
-		<p>This privacy policy is in effect since the date of its last update indicated above, and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to change this privacy policy at any time.</p>
+		<h3>Revisions</h3>
+		<p>We reserve the right to revise the privacy policy at our sole discretion at any time.</p>
+		<p>This privacy policy has been in effect since the date of its last revision, indicated above. It will remain in effect except with respect to any revisions in the future, which will take effect immediately after being posted on this page. If a revision is material, we will try to give at least 30 days’ notice before it takes effect.</p>
 	</article>
 </template>
--- src/pages/Terms.vue
+++ src/pages/Terms.vue
@@ -2,23 +2,24 @@
 	<article class="solo-message">
 		<h2>Terms of Service</h2>
 
-		<p><i>Last updated on March 27, 2021</i></p>
+		<p><i>Last revised on March 27, 2021</i></p>
 
-		<p>Please read these terms of service (“Terms”) before using this website, MTG Deck Organizer (“MDO”), operated by Iristorm Design (“us,” “we,” “our”). These Terms apply to anyone who uses MDO.</p>
-		<p>By using MDO, you agree to be bound by these Terms. If you disagree with any part of the Terms, do not use MDO.</p>
+		<p>Please read these terms of service before using this website, MTG Deck Organizer (“MDO”), operated by Iristorm Design (“us,” “we,” “our”).</p>
+		<p>By using MDO, you agree to be bound by the terms of service and the <router-link to="/privacy">privacy policy</router-link>. If you disagree with any part of the terms of service, do not use MDO.</p>
 
 		<h3>Limitations of Liability</h3>
-		<p>MDO is provided “as-is.” We may change or remove access to any aspect of MDO at any time without notice.</p>
-		<p>MDO is provided on an “as-available” basis. It may be subject to outages or periods of slow activity at times.</p>
+		<p>MDO is provided “as-is.” We may change or remove any aspect of the site at any time without warning.</p>
+		<p>MDO is provided on an “as-available” basis. It may be subject to outages or periods of slow activity at unexpected times.</p>
 		<p>MDO links to and interacts with third-party services. We have no control over, and assume no liability for, the content, policies, or practices of those services.</p>
 		<p>You use MDO at your own risk. You agree that we do not owe you any compensation for site outages, software bugs, inconveniences, or our failure to live up to any statement in this document.</p>
 		<p>You further agree that you will not hold us liable for any losses or damages, whether those be personal, financial, data, competitive, or otherwise, arising from your use of MDO.</p>
 
 		<h3>Unacceptable Use</h3>
-		<p>You may not attempt to disrupt, tamper with, or place undue burden on MDO’s web servers, whether manually or via automation.</p>
-		<p>If you breach any provision of these Terms, we may terminate your access to MDO without prior notice or liability.</p>
+		<p>Do not attempt to disrupt, tamper with, or place undue burden on MDO’s web servers, whether manually or via automation.</p>
+		<p>If you breach any provision of these terms of service, we may terminate your access to MDO without warning or liability.</p>
 
-		<h3>Changes to Terms</h3>
-		<p>We reserve the right to change MDO’s Terms at our sole discretion at any time. If a change is material, we will try to provide at least 30 days’ notice prior to the new Terms taking effect.</p>
+		<h3>Revisions</h3>
+		<p>We reserve the right to revise the terms of service at our sole discretion at any time.</p>
+		<p>These terms of service have been in effect since the date of their last revision, indicated above. They will remain in effect except with respect to any revisions in the future, which will take effect immediately after being posted on this page. If a revision is material, we will try to give at least 30 days’ notice before it takes effect.</p>
 	</article>
 </template>
