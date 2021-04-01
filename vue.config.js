761c665ed31e93a45f49610761bb84ea728a2f6e
Damian Schmidt <damian@iristormdesign.com>
2021-04-01 16:05:37 -0400

    Some PWA manifest setup

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = {
 		index: {
 			entry: 'src/main.js'
 		}
+	},
+	pwa: {
+		name: 'MTG Deck Organizer',
+		themeColor: '#ea4b12',
+		msTileColor: '#272e31',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black'
 	}
 }
