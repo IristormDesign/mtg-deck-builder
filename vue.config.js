6d63605cde1485aba662f507ae163131413fedbd
Damian Schmidt <damian@iristormdesign.com>
2021-04-06 16:22:45 -0400

    Favicons and app icons

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,24 @@ module.exports = {
 		themeColor: '#ea4b12',
 		msTileColor: '#272e31',
 		appleMobileWebAppCapable: 'yes',
-		appleMobileWebAppStatusBarStyle: 'black'
+		manifestOptions: {
+			icons: [
+				{
+					src: './img/icons/android-chrome-192x192.png',
+					type: 'image/png',
+					sizes: '192x192'
+				},
+				{
+					src: './img/icons/android-chrome-512x512.png',
+					type: 'image/png',
+					sizes: '512x512'
+				}
+			]
+		},
+		iconPaths: {
+			appleTouchIcon: 'img/icons/apple-touch-icon.png',
+			maskIcon: null,
+			msTileImage: 'img/icons/mstile-150x150.png'
+		}
 	}
 }
