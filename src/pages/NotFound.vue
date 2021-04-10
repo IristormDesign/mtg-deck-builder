846c3b3669d75b3fea1f69a1592b76d7291f5432
Damian Schmidt <damian@iristormdesign.com>
2021-04-09 20:31:34 -0400

    M

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -4,7 +4,7 @@
 
 		<h2>Page Not Found</h2>
 
-		<p>Your web browser tried to go to a page that doesn’t exist. A page might have existed before but has since been removed.</p>
+		<p>Your web browser tried to go to a page that doesn’t exist. If that page had existed before, it has since been removed.</p>
 
 		<p><small>ILLUSTRATION: “<a href="https://scryfall.com/card/sok/84/one-with-nothing" target="_blank" rel="noopener noreferrer">One with Nothing</a>” by Jim Nelson</small></p>
 	</article>
