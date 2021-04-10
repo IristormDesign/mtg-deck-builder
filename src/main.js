76a9f0b656da06bb419d9b13b86b52e180a9d5c0
Damian Schmidt <damian@iristormdesign.com>
2021-04-09 22:25:31 -0400

    Applied Google Analytics tag

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import store from './store.js'
 import router from './router.js'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
 import './registerServiceWorker'
+
+Vue.use(VueGtag, {
+	config: { id: 'UA-60677306-1' }
+}, router)
 
 new Vue({
 	store,
