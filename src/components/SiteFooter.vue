d1d845744603f11f0dd7bf6c26b8d8e154128d49
Damian Schmidt <damian@iristormdesign.com>
2021-04-17 22:37:36 -0400

    Misc

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,9 +1,12 @@
 <template>
 	<footer class="site-footer">
 		<div class="wrap">
-			<p><strong>MTG Deck Builder</strong> by Iristorm Design — Version 1.0.3 <span>|</span> <router-link to="/terms">Terms of Service</router-link> <span>|</span> <router-link to="/privacy">Privacy Policy</router-link> <span>|</span> <a href="#clear" @click.prevent="clearLocalStorage()">Clear Stored Data</a></p>
+			<p><router-link to="/"><strong>MTG Deck Builder</strong></router-link> by <a href="https://iristormdesign.com" target="_blank" rel="noopener">Iristorm Design</a> — Version 1.0.3 <span>|</span> <router-link to="/terms">Terms of Service</router-link> <span>|</span> <router-link to="/privacy">Privacy Policy</router-link> <span>|</span> <a href="#clear" @click.prevent="clearLocalStorage()">Clear Stored Data</a></p>
+
 			<p><i>Magic: The Gathering</i> content is TM and &copy; by Wizards of the Coast. This app is not affiliated with or endorsed by Wizards of the Coast, but is allowed under their Fan Content Policy.</p>
+
 			<p>Data and images of <i>Magic</i> cards are provided by the Scryfall API. This app is not affiliated with or endorsed by Scryfall.</p>
+
 			<p>All other content, unless indicated otherwise, is copyright &copy; 2021 by Damian Schmidt.</p>
 		</div>
 	</footer>
@@ -13,7 +16,7 @@
 export default {
 	methods: {
 		clearLocalStorage () {
-			const confirmed = confirm('⚠ Do you want to permanently erase all decks you’ve made here? (The app’s default decks will be restored.)')
+			const confirmed = confirm('⚠ You’re about to permanently delete all of your stored personal data from this app, which means you’ll lose the customized decks you’ve made here. Are you sure you want to do this?')
 
 			if (confirmed) {
 				localStorage.clear()
