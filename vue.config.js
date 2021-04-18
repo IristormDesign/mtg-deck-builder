e3b35bfe34512416e6c9da454e40696c8f621cf1
Damian Schmidt <damian@iristormdesign.com>
2021-04-18 15:23:47 -0400

    Misc CSS

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	},
 	pwa: {
 		name: 'MTG Deck Builder',
-		themeColor: '#c34619', // The color of the site header's bottom border.
+		themeColor: '#ba4218', // The color of the site header's bottom border.
 		msTileColor: '#272e31',
 		appleMobileWebAppCapable: 'yes',
 		manifestOptions: {
