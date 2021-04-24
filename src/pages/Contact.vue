5d830b1609f7baf52b97db814bbf379a249fdc65
Damian Schmidt <damian@iristormdesign.com>
2021-04-23 22:16:27 -0400

    Adding more links to Contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,8 +4,8 @@
 
 		<p>You can email the developer of this web app, Damian Schmidt, at <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>. Any comments or questions are appreciated.</p>
 
-		<!-- <p>MTG Deck Builder is open-source and its files are available on <a href="https://github.com">GitHub</a>.</p> -->
+		<p>MTG Deck Builder is open-source and its files are viewable on <a href="https://github.com/IristormDesign/mtg-deck-builder" target="_blank" rel="noopener">GitHub</a>.</p>
 
-		<!-- <p>To learn about how this web app was created, <a href="https://iristormdesign.com/portfolio/mtg-deck-builder" target="_blank" rel="noopener">read the article about it</a> at Iristorm Design.</p> -->
+		<p>To learn about how this web app was created, <a href="https://iristormdesign.com/portfolio/mtg-deck-builder" target="_blank" rel="noopener">read the article about it</a> at Iristorm Design.</p>
 	</article>
 </template>
