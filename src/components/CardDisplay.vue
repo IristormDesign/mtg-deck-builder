4e498f1f7b42357f2469e03749dfa6cf36282d4d
Damian Schmidt <damian@iristormdesign.com>
2021-06-04 23:15:06 -0400

    Code optimization

--- src/components/CardDisplay.vue
+++ src/components/CardDisplay.vue
@@ -1,27 +1,24 @@
 <template>
 	<section class="card-display">
-		<div v-for="card in deck.cards" :key="card.name">
-			<transition
-				name="card-browse"
-				appear
-				appear-active-class="card-browse-appear-active"
+		<transition
+			v-for="card in deck.cards" :key="card.name"
+			name="card-browse" appear appear-active-class="card-browse-appear-active"
+		>
+			<a
+				v-if="deck.viewedCard === card.name"
+				:key="card.name" :class="card.colors[0]" :href="card.link"
+				target="_blank" rel="noopener noreferrer"
+				title="Click to open this card’s page on Scryfall"
 			>
-				<a
-					v-if="deck.viewedCard === card.name"
-					:key="card.name" :class="card.colors[0]" :href="card.link"
-					target="_blank" rel="noopener noreferrer"
-					title="Click to open this card’s page on Scryfall"
-				>
-					<div class="loading-indicator">
-						Loading image&hellip;
-					</div>
-					<img
-						:src="card.img" :alt="card.name"
-						width="488" height="680"
-					/>
-				</a>
-			</transition>
-		</div>
+				<div class="loading-indicator">
+					Loading image&hellip;
+				</div>
+				<img
+					:src="card.img" :alt="card.name"
+					width="488" height="680"
+				/>
+			</a>
+		</transition>
 	</section>
 </template>
 
