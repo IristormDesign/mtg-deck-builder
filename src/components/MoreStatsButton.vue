c688c590c564988498e3ef2fe82da53f2288e08c
Damian Schmidt <damian@iristormdesign.com>
2021-07-13 13:49:03 -0400

    Conditionally disable More Stats button

--- src/components/MoreStatsButton.vue
+++ src/components/MoreStatsButton.vue
@@ -1,14 +1,23 @@
 <template>
 	<section class="deck-info more-stats-button">
-		<router-link :to="{
-			name: 'moreStats',
-			params: {
-				deckPath: deck.path,
-				deck: deck
-			},
-		}">
+		<router-link
+			v-if="this.deck.cards.length > 0"
+			:to="{
+				name: 'moreStats',
+				params: {
+					deckPath: deck.path,
+					deck: deck
+				},
+			}">
 			More Statistics
 		</router-link>
+		<button
+			v-else
+			disabled
+			title="Add at least one card to the deck to view more statistics."
+		>
+			More Statistics
+		</button>
 	</section>
 </template>
 
