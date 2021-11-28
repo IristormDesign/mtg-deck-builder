2de998a22edaf0b1972a6f7171d11d58e13869e7
Damian Schmidt <damian@iristormdesign.com>
2021-11-28 13:35:00 -0500

    Mana symbol titles

--- src/js/store.js
+++ src/js/store.js
@@ -22,11 +22,11 @@ export default new Vuex.Store({
 		showDeckMenu: false,
 		sortAttribute: 'type',
 		manaSymbol: {
-			w: '<span class="mana-symbol white" title="White mana"><div>W</div></span>',
-			u: '<span class="mana-symbol blue" title="Blue mana">U</span>',
-			b: '<span class="mana-symbol black" title="Black mana">B</span>',
-			r: '<span class="mana-symbol red" title="Red mana">R</span>',
-			g: '<span class="mana-symbol green" title="Green mana">G</span>'
+			w: '<span class="mana-symbol white" title="White mana symbol"><div>W</div></span>',
+			u: '<span class="mana-symbol blue" title="Blue mana symbol">U</span>',
+			b: '<span class="mana-symbol black" title="Black mana symbol">B</span>',
+			r: '<span class="mana-symbol red" title="Red mana symbol">R</span>',
+			g: '<span class="mana-symbol green" title="Green mana symbol">G</span>'
 		},
 		raritySymbol: {
 			c: '<div class="rarity-symbol common" title="Common">C</div>',
