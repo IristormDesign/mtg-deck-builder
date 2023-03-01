f96c0beac6d6515d4fef9c840963f260418914f6
Damian Schmidt <damian@iristormdesign.com>
2023-02-28 21:14:11 -0500

    Minor app footer adjustments

--- src/views/ManualPage.vue
+++ src/views/ManualPage.vue
@@ -279,8 +279,8 @@
 
 			<section id="m-app-dev">
 				<h3>App Development</h3>
-				<p>MTG Deck Builder has been designed and developed by Damian Schmidt of <a href="https://iristormdesign.com" target="_blank" rel="noopener">Iristorm Design</a>. If you’re interested, you can <a href="https://iristormdesign.com/portfolio/mtg-deck-builder" target="_blank" rel="noopener">learn about the nuts and bolts</a> of the app.</p>
-				<p>Do you still have questions about MTG Deck Builder, or have any comments (whether good or bad) to share with the app developer? Email Damian Schmidt at <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>.</p>
+				<p>MTG Deck Builder has been designed and developed by Damian Schmidt of <a href="https://iristormdesign.com" target="_blank" rel="noopener">Iristorm Design</a>. If you’re interested, you can read about <a href="https://iristormdesign.com/portfolio/mtg-deck-builder" target="_blank" rel="noopener">how this app was made</a>.</p>
+				<p>Do you still have questions about MTG Deck Builder, or have any comments (whether good or bad) for the app developer? Email Damian Schmidt at <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>.</p>
 			</section>
 		</div>
 	</article>
