eeeb86d2248e3de541a340569f337485c4d2d05a
Damian Schmidt <damian@iristormdesign.com>
2023-07-04 11:27:40 -0400

    Made app menu links taller on mobile

--- src/components/ManualIntroduction.vue
+++ src/components/ManualIntroduction.vue
@@ -6,7 +6,7 @@
 		</figure>
 		<p>This document is the user manual for <router-link :to="{name: 'home'}">MTG Deck Builder by Iristorm Design</router-link>. Here you can learn the details about how this web app works.</p>
 		<p>The manual is divided into many chapters. You can skip directly to a chapter you want to read by clicking on the relevant link in the table of contents, or on certain other links throughout the manual.</p>
-		<p>The manual displays screenshots of parts of MTG Deck Builder. Note that some of these screenshots show the app in an older version, which may look slightly different from the current version.</p>
+		<p>The manual has screenshots that show parts of MTG Deck Builder. Note that some screenshots show the app in an older version, which may look slightly different from the current version.</p>
 		<p>MTG Deck Builder assumes you know how to play <i>Magic: The Gathering</i>. If you have an issue about the rules of the game, you can refer to them on the <a href="https://magic.wizards.com/en/rules" target="_blank">official <i>Magic</i> website</a> or the <a href="https://mtg.fandom.com/wiki/Comprehensive_Rules" target="_blank">Fandom MTG Wiki</a>.</p>
 		<p>When you start MTG Deck Builder for the first time ever, it provides two sample decks (“Boros Equipment” and “Sultai Ramp”). You may modify these decks however you like or delete them.</p>
 
