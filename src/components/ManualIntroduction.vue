b36192df906658cc822ea870408945cd581829c3
Damian Schmidt <damian@iristormdesign.com>
2023-07-03 23:46:41 -0400

    Fixed up appearances of form inputs

--- src/components/ManualIntroduction.vue
+++ src/components/ManualIntroduction.vue
@@ -5,8 +5,9 @@
 			<small>(Illustration: <i><a href="https://scryfall.com/card/dom/235/urzas-tome" target="_blank" rel="noopener noreferrer">Urza’s Tome</a></i> by Aaron Miller)</small>
 		</figure>
 		<p>This document is the user manual for <router-link :to="{name: 'home'}">MTG Deck Builder by Iristorm Design</router-link>. Here you can learn the details about how this web app works.</p>
-		<p>The user manual is divided into many chapters. You can skip directly to a chapter you want to read by clicking on the relevant link in the table of contents, or on certain other links throughout the manual.</p>
-		<p>MTG Deck Builder assumes you know how to play <i>Magic: The Gathering</i>. If you have an issue about the game’s rules, you can refer to them on the <a href="https://magic.wizards.com/en/rules" target="_blank" rel="noopener noreferrer">official <i>Magic</i> website</a> or the <a href="https://mtg.fandom.com/wiki/Comprehensive_Rules" target="_blank" rel="noopener">Fandom MTG Wiki</a>.</p>
+		<p>The manual is divided into many chapters. You can skip directly to a chapter you want to read by clicking on the relevant link in the table of contents, or on certain other links throughout the manual.</p>
+		<p>The manual displays screenshots of parts of MTG Deck Builder. Note that some of these screenshots show the app in an older version, which may look slightly different from the current version.</p>
+		<p>MTG Deck Builder assumes you know how to play <i>Magic: The Gathering</i>. If you have an issue about the rules of the game, you can refer to them on the <a href="https://magic.wizards.com/en/rules" target="_blank" rel="noopener noreferrer">official <i>Magic</i> website</a> or the <a href="https://mtg.fandom.com/wiki/Comprehensive_Rules" target="_blank" rel="noopener">Fandom MTG Wiki</a>.</p>
 		<p>When you start MTG Deck Builder for the first time ever, it provides two sample decks (“Boros Equipment” and “Sultai Ramp”). You may modify these decks however you like or delete them.</p>
 
 		<manual-symbols />
