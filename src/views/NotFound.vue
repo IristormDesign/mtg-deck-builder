4dfd7e104a453efde1853832fc5d77a9f8a5a791
Damian Schmidt <damian@iristormdesign.com>
2023-07-18 23:15:47 -0400

    Added advice on 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,6 +5,7 @@
 			<img class="intro-illustration" src="~@/img/one-with-nothing.jpg" width="616" height="412" alt="One with Nothing" />
 			<figcaption>(Illustration: <i><a href="https://scryfall.com/card/sok/84/one-with-nothing" target="_blank">One with Nothing</a></i> by Jim Nelson)</figcaption>
 		</figure>
-		<p>Your web browser tried to go to a page that doesn’t exist. If that page existed before, it has since been removed.</p>
+		<p><strong>Your web browser tried to go to a page that doesn’t exist.</strong> If that page existed before, it has since been removed.</p>
+		<p>Did you arrive on this error page expecting to see a deck page created by another user? If so, ask that user to export his or her deck as a deck data file and send it to you. For more info, <router-link to="/manual#m-storage-of-deck-data">see the user manual on storage of deck data</router-link>.</p>
 	</article>
 </template>
