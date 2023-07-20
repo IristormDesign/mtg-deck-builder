66ed671a3c91272632c22e993d9ced1d4977fcf9
Damian Schmidt <damian@iristormdesign.com>
2023-07-20 11:54:07 -0400

    Styled scroll bars (Webkit)

--- src/components/manual-chapters/ManualCardQuantities.vue
+++ src/components/manual-chapters/ManualCardQuantities.vue
@@ -6,14 +6,14 @@
 				src="@/img/manual-card-quantities.jpg" width="118" height="256"
 				alt="Snippet of card quantities" title="Screenshot" loading="lazy" />
 		</p>
-		<p>To the right of each card button in the <a href="#m-card-list">card list</a> is a number that represents the card’s quantity (“QTY.”) in your deck’s active card list.</p>
+		<p>To the right of each card button in the <a href="#m-card-list">card list</a> is a number representing the quantity (“QTY.”) of the card.</p>
 		<p>To change a card’s quantity, do any of the following methods:</p>
 		<ul>
 			<li>Click on the quantity number, then use your keyboard to type in a new number.</li>
-			<li>If you’re on a larger screen device, hover your mouse pointer over the quantity number to make a pair of buttons appear beside it. Of that pair of buttons, click on the “+” (plus sign) to increase the card’s quantity, or the “&minus;” (minus sign) to decrease.</li>
-			<li>Click on the quantity number to make your web browser focus on it, then press the up-arrow key on your keyboard to increase, or the down-arrow key to decrease.</li>
+			<li>If you’re on a large-screen device, hover your mouse pointer over the quantity number to make a pair of buttons appear beside it. Of that pair of buttons, click on the “+” (plus sign) to increase the card’s quantity, or the “&minus;” (minus sign) to decrease.</li>
+			<li>Click on the quantity number to make your web browser focus on it, and then press the up-arrow key on your keyboard to increase, or the down-arrow key to decrease.</li>
 		</ul>
-		<p>To remove a card from your deck, decrease the card’s quantity to 0 (zero), or delete the quantity number. Before the card actually gets removed, the app asks you to confirm the removal. If you choose to cancel instead, then the card stays in your deck with its quantity reset to 1.</p>
-		<p>Following the rules of <i>Magic</i>, each card of a particular name in a deck—except cards of the “basic land” type—cannot be set to a quantity greater than 4.</p>
+		<p>To remove a card from the card list, decrease the card’s quantity to 0, or delete the quantity number. Before the card actually gets removed, the app asks you to confirm the removal. If you choose to cancel instead, then the card stays in your deck with its quantity reset to 1.</p>
+		<p>Conforming to the rules of <i>Magic</i>, each card of a particular name in a deck—except cards of the “basic land” type—cannot be set to a quantity greater than 4.</p>
 	</section>
 </template>
