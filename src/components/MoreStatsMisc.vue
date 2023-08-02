42e0280b2179680cbe434cc7ceb8c6ebea4ac141
Damian Schmidt <damian@iristormdesign.com>
2023-08-02 16:04:29 -0400

    Some code optimization

--- src/components/MoreStatsMisc.vue
+++ src/components/MoreStatsMisc.vue
@@ -40,13 +40,13 @@ export default {
 			if (this.emptyTable) {
 				markup += this.tableBodyEmpty
 			} else {
-				this.miscProperties.forEach(heading => {
-					const count = this.countMisc(heading)
+				this.miscProperties.forEach(prop => {
+					const count = this.countMisc(prop)
 
 					if (count > 0) {
 						markup += `
 							<tr>
-								<th>${heading}</th>
+								<th>${prop}</th>
 								<td>${count}</td>
 								<td>${this.calculatePercentage(count)}</td>
 							</tr>
@@ -58,32 +58,54 @@ export default {
 			return markup
 		},
 		countMisc (prop) {
-			prop = prop.toLowerCase()
+			switch (prop.toLowerCase()) {
+				case 'basic land':
+					return this.cardsOfProp(matchesBasicLand)
+				case 'legendary':
+					return this.cardsOfProp(matchesLegendary)
+				case 'monocolored':
+					return this.cardsOfProp(matchesMonocolored)
+				case 'multicolored':
+					return this.cardsOfProp(matchesMulticolored)
+				case 'variable cost':
+					return this.cardsOfProp(matchesVariableCost)
+				case 'variable p/t':
+					return this.cardsOfProp(matchesVariablePT)
+				case 'double-faced':
+					return this.cardsOfProp(matchesDoubleFaced)
+			}
+
+			function matchesBasicLand (card) {
+				const regexBasicLand = /\bBasic (\w* )?Land\b/
+				return regexBasicLand.test(card.type)
+			}
+			function matchesLegendary (card) {
+				const regexLegendary = /\bLegendary\b/
+				return regexLegendary.test(card.type)
+			}
+			function matchesMonocolored (card) {
+				return card.colors.length === 1
+			}
+			function matchesMulticolored (card) {
+				return card.colors[0] === 'multicolor'
+			}
+			function matchesVariableCost (card) {
+				const regexVariableCost = /\{X\}/
+				return regexVariableCost.test(card.mana)
+			}
+			function matchesVariablePT (card) {
+				return card.power === '*' || card.toughness === '*'
+			}
+			function matchesDoubleFaced (card) {
+				const regexDoubleFaced = /\w\s\/\s\w/
+				return regexDoubleFaced.test(card.name)
+			}
+		},
+		cardsOfProp (propMatcher) {
 			let count = 0
 
 			this.deck.cards.forEach(card => {
-				const regexBasicLand = /\bBasic (\w* )?Land\b/
-				const regexLegendary = /\bLegendary\b/
-				const regexVariableCost = /\{X\}/
-				const regexDoubleFaced = /\w\s\/\s\w/
-
-				const matchesBasicLand = regexBasicLand.test(card.type)
-				const matchesLegendary = regexLegendary.test(card.type)
-				const matchesMonocolored = card.colors.length === 1
-				const matchesMulticolored = card.colors[0] === 'multicolor'
-				const matchesVariableCost = regexVariableCost.test(card.mana)
-				const matchesVariablePT = card.power === '*' || card.toughness === '*'
-				const matchesDoubleFaced = regexDoubleFaced.test(card.name)
-
-				if (
-					(prop === 'basic land' && matchesBasicLand) ||
-					(prop === 'legendary' && matchesLegendary) ||
-					(prop === 'monocolored' && matchesMonocolored) ||
-					(prop === 'multicolored' && matchesMulticolored) ||
-					(prop === 'variable cost' && matchesVariableCost) ||
-					(prop === 'variable p/t' && matchesVariablePT) ||
-					(prop === 'double-faced' && matchesDoubleFaced)
-				) {
+				if (propMatcher(card)) {
 					for (let i = 0; i < card.qty; i++) {
 						count++
 					}
