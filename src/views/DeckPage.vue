ee52513655bdccf919399c12d67ac48e91a3cd7c
Damian Schmidt <damian@iristormdesign.com>
2023-08-09 01:12:12 -0400

    Fixed deck version bug

--- src/views/DeckPage.vue
+++ src/views/DeckPage.vue
@@ -37,38 +37,67 @@ import UpdateDataNotice from '@/components/DeckUpdateDataNotice.vue'
 export default {
 	components: { DeckName, DeckColors, AverageManaValue, DateEdited, CardNames, CardTotal, MoreStatsButton, UpdateDataNotice },
 	mixins: [getActiveDeck],
+	data () {
+		return {
+			dataModified: false
+		}
+	},
 	beforeRouteUpdate (to, from, next) {
 		this.$store.commit('setShowSideboard', false)
 
 		next()
+		this.prepareDecksWithOutdatedData()
 	},
 	created () {
 		this.$store.commit('setShowSideboard', false)
 
-		this.addSideboardObject()
+		this.prepareDecksWithOutdatedData()
 	},
 	methods: {
-		/**
-		 * Add the `sideboard` object property for any decks that are missing it. (Decks created from earlier app versions didn't have sideboards.)
-		 */
-		addSideboardObject () {
-			let dataModified = false
-
-			this.$store.state.decks.forEach(deck => {
-				if (!deck.sideboard) {
-					deck.sideboard = {
-						cards: [],
-						viewedCard: ''
-					}
-					dataModified = true
-				}
-			})
+		prepareDecksWithOutdatedData () {
+			this.addMissingSideboard()
+			this.checkForDataVersion()
 
 			this.$nextTick(() => {
-				if (dataModified) {
+				if (this.dataModified) {
 					this.$store.commit('setDecks', this.$store.state.decks)
 				}
 			})
+		},
+		/**
+		 * If the deck's data version number doesn't match the latest version number, then determine what its version number should be.
+		 */
+		checkForDataVersion () {
+			if (this.deck.dataVersion !== this.$store.state.latestDeckDataVersion) {
+				this.deck.dataVersion = this.determinedDataVersion()
+
+				this.dataModified = true
+			}
+		},
+		/**
+		 * @returns {number} Version number
+		 */
+		determinedDataVersion () {
+			for (const card of this.deck.cards) {
+				if (!card.keywords) { // For any card, even one that has no keywords, its `card` object should have a `keywords` key assigned to an array, whether empty or not. Also, the `power` and `toughness` keys were added at the same time as the `keywords` key, so if `keywords` doesn't exist, then neither do `power` or `toughness`.
+					return 1
+				}
+			}
+			return this.$store.state.latestDeckDataVersion
+
+			// The sideboard's cards are intentionally not checked for the `keywords` key, because the sideboard feature had been released after `keywords`.
+		},
+		/**
+		 * Add the `sideboard` object for any decks that are missing it. (Decks created from earlier app versions didn't have sideboards.)
+		 */
+		addMissingSideboard () {
+			if (!this.deck.sideboard) {
+				this.deck.sideboard = {
+					cards: [],
+					viewedCard: ''
+				}
+				this.dataModified = true
+			}
 		}
 	}
 }
