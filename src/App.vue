5282c9dad317d62e058a451209e8704a593be0fb
Damian Schmidt <damian@iristormdesign.com>
2023-08-13 01:18:02 -0400

    Optimized code

--- src/App.vue
+++ src/App.vue
@@ -23,38 +23,45 @@ export default {
 			if (this.$route.name === 'home') {
 				return 'root-home-page'
 			} else {
-				return null
+				return 'texture-bg' // For better performance, don't let the background texture image load on the home page.
 			}
 		}
 	},
 	mounted () {
-		this.$watch(function () {
-			// For better performance, don't let the background texture image load on the home page.
-			if (this.$route.name !== 'home') {
-				document.querySelector('.fade-bg').classList.add('texture-bg')
-			}
-		})
-
-		// Prevent all links from opening a new tab via middle-click, with the exception of the links that are always set to open in a new tab.
-		document.addEventListener('auxclick', (event) => {
-			function conditions (el) {
-				if (el !== null) {
-					return (el.matches('a') && el.getAttribute('target') !== '_blank')
+		this.preventMiddleClicking()
+		this.loadDefaultDecks()
+	},
+	methods: {
+		/**
+		 * Prevent all links from opening a new tab via middle-click, with the exception of the links that are always set to open in a new tab.
+		 */
+		preventMiddleClicking () {
+			document.addEventListener('auxclick', (event) => {
+				function conditions (el) {
+					if (el !== null) {
+						return (el.matches('a') && el.getAttribute('target') !== '_blank')
+					}
 				}
-			}
 
-			if (conditions(event.target) || conditions(event.target.closest('a'))) {
-				event.preventDefault()
+				if (
+					conditions(event.target) ||
+					conditions(event.target.closest('a'))
+				) {
+					event.preventDefault()
+				}
+			})
+		},
+		/**
+		 * Load the two default decks if needed.
+		 */
+		loadDefaultDecks () {
+			if (this.$store.state.loadDefaultDecks) {
+				import('@/js/default-decks.json')
+					.then(data => {
+						this.$store.commit('setDecks', data.decks)
+						this.$store.commit('setLoadDefaultDecks', false)
+					})
 			}
-		})
-
-		// Load the two default decks if needed.
-		if (this.$store.state.loadDefaultDecks) {
-			import('@/js/default-decks.json')
-				.then(data => {
-					this.$store.commit('setDecks', data.decks)
-					this.$store.commit('setLoadDefaultDecks', false)
-				})
 		}
 	}
 }
