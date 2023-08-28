98a2f44d5cb0ae325c97f40f412fa9f35cb2ff8c
Damian Schmidt <damian@iristormdesign.com>
2023-08-27 21:56:08 -0400

    Styled "Delete Decks" page

--- src/views/DeleteDecks.vue
+++ src/views/DeleteDecks.vue
@@ -4,35 +4,49 @@
 		class="delete-decks content-box"
 	>
 		<h2>Delete Decks</h2>
-		<template v-if="decks.length <= 0">
+		<template v-if="numExisting <= 0">
 			<p>You have no more decks. <router-link :to="{name: 'createDeck'}">(Create one?)</router-link></p>
 		</template>
 		<template v-else>
-			<p>In the checklist below, select the decks that you want permanently erased, then click on the Delete Selected button.</p>
+			<p>In the checklist below, select the decks that you want to remove, then click on the Delete Selected button. Warning: Deleted decks cannot be restored.</p>
 			<form>
 				<div
 					class="multi-select-buttons"
-					v-show="decks.length >= 3"
+					v-show="numExisting >= 4"
 				>
-					<button @click="selectAll()" type="button">Select All</button>
-					<button @click="selectNone()" type="button">Select None</button>
+					<div class="button-container">
+						<button
+							@click="selectAll()"
+							:disabled="numChecked === numExisting"
+							type="button"
+						>Select All</button>
+					</div>
+					<div class="button-container">
+						<button
+							@click="selectNone()"
+							:disabled="numChecked === 0"
+							type="button"
+						>Deselect All</button>
+					</div>
 				</div>
-				<p><strong>Your Decks:</strong></p>
 				<ul class="checklist">
-					<li v-for="deck in decks" :key="deck.name">
+					<li v-for="deck in $store.state.decks" :key="deck.name">
 						<input type="checkbox" :id="deck.name" :value="deck.name" v-model="checkedDecks">
 						<label :for="deck.name">{{ deck.name }}</label>
 					</li>
 				</ul>
-				<p class="button-container">
-					<button @click.prevent="handleSubmit()">Delete Selected</button>
+				<p class="button-container submit-button">
+					<button
+						@click.prevent="handleSubmit()"
+						:disabled="numChecked <= 0"
+					>Delete Selected</button>
 				</p>
 			</form>
 		</template>
 	</article>
 	<article
 		v-else
-		class="deck-deleted content-box"
+		class="decks-deleted content-box"
 		@click="$store.getters.attentionHeaderButton"
 	>
 		<figure>
@@ -55,15 +69,18 @@ export default {
 		}
 	},
 	computed: {
-		decks () {
-			return this.$store.state.decks
-		},
 		deletedDecksMessage () {
-			if (this.checkedDecks.length === 1) {
+			if (this.numChecked === 1) {
 				return 'That deck is now deleted.'
 			} else {
 				return 'Those decks are now deleted.'
 			}
+		},
+		numChecked () {
+			return this.checkedDecks.length
+		},
+		numExisting () {
+			return this.$store.state.decks.length
 		}
 	},
 	mounted () {
@@ -92,26 +109,22 @@ export default {
 			}
 		},
 		confirmedDelete () {
-			const numOfChecked = this.checkedDecks.length
-
-			if (numOfChecked <= 0) {
-				alert('You haven’t selected any deck to delete.')
-			} else if (numOfChecked === 1) {
+			if (this.numChecked === 1) {
 				return confirm(
 					`⚠ Are you sure you want to delete “${this.checkedDecks[0]}”?`)
-			} else if (numOfChecked === 2) {
+			} else if (this.numChecked === 2) {
 				return confirm(
 					`⚠ Are you sure you want to delete “${this.checkedDecks[0]}” and “${this.checkedDecks[1]}”?`)
-			} else if (numOfChecked === this.decks.length) {
+			} else if (this.numChecked === this.numExisting) {
 				return confirm(
-					`⚠ Are you sure you want to delete all ${this.decks.length} of your decks?`)
+					`⚠ Are you sure you want to delete all ${this.numExisting} of your decks?`)
 			} else {
 				return confirm(
-					`⚠ Are you sure you want to delete the ${numOfChecked} decks you’ve selected?`)
+					`⚠ Are you sure you want to delete the ${this.numChecked} decks you’ve selected?`)
 			}
 		},
 		removeSelectedDecks () {
-			let decks = this.decks
+			let decks = this.$store.state.decks
 
 			for (const checkedDeckName of this.checkedDecks) {
 				decks = decks.filter(
@@ -121,20 +134,6 @@ export default {
 
 			this.isDeleted = true
 			this.$store.commit('setDecks', decks)
-
-			// (function () {
-			// 	const nameList = []
-
-			// 	if (decks.length > 0) {
-			// 		decks.forEach(deck => {
-			// 			nameList.push(deck.name)
-			// 		})
-
-			// 		console.log('> REMAINING DECKS:\n' + nameList.join(', '))
-			// 	} else {
-			// 		console.log('> ALL DECKS DELETED')
-			// 	}
-			// })()
 		}
 	}
 }
