cae23e5ed45df664f3e391dcfd23eeac3122636b
Damian Schmidt <damian@iristormdesign.com>
2023-10-04 22:48:23 -0400

    Added Scryfall opener button

---
+++ src/components/DeckScryfallOpener.vue
@@ -0,0 +1,7 @@
+<template>
+	<section class="scryfall-opener">
+		<a href="https://scryfall.com/advanced" target="_blank" title="Open Scryfall advanced search">
+			<img src="@/img/scryfall-logo.svg" alt="Scryfall logo" />
+		</a>
+	</section>
+</template>
--- src/views/DeckMain.vue
+++ src/views/DeckMain.vue
@@ -8,6 +8,7 @@
 			</div>
 			<card-list :deck="deck" />
 			<div class="deck-control-panel action-controls">
+				<scryfall-opener />
 				<card-adder :deck="deck" />
 				<deck-actions :deck="deck" />
 			</div>
@@ -20,12 +21,13 @@ import CardDisplay from '@/components/DeckCardDisplay.vue'
 import CardSorter from '@/components/DeckCardSorter.vue'
 import CardGroupSwitch from '@/components/DeckCardGroupSwitch.vue'
 import CardList from '@/components/DeckCardList.vue'
+import ScryfallOpener from '@/components/DeckScryfallOpener.vue'
 import CardAdder from '@/components/DeckCardAdder.vue'
 import DeckActions from '@/components/DeckDeckActions.vue'
 import getActiveDeck from '@/mixins/getActiveDeck.js'
 
 export default {
-	components: { CardDisplay, CardSorter, CardGroupSwitch, CardList, CardAdder, DeckActions },
+	components: { CardDisplay, CardSorter, CardGroupSwitch, CardList, ScryfallOpener, CardAdder, DeckActions },
 	mixins: [getActiveDeck]
 }
 </script>
