98b16d7382c7346425842bb8d39d3b66be1a141b
Damian Schmidt <damian@iristormdesign.com>
2023-10-11 00:13:30 -0400

    Color tweaks on links for better accessibility

--- src/components/manual-chapters/ManualDeckActions.vue
+++ src/components/manual-chapters/ManualDeckActions.vue
@@ -13,11 +13,12 @@
 				<h5>Export</h5>
 				<p>You’re taken to the Export Decks page. There you can select any of your decks in MTG Deck Builder to “export,” which means to have the app generate a <a href="#m-deck-data-files">deck data file</a> for those decks. The file is saved on your computer or device to wherever your web browser puts downloads.</p>
 				<p>Once you have a deck data file, you—or anyone whom you give the file to—can “<a href="#m-create-deck">import</a>” it, which means to have the app recreate the certain decks from the data contained in that file.</p>
-				<p>If you export a single deck alone, the deck data file is named after that deck’s name. If you export multiple decks together, the file is named “My Decks.” In either case, the file has “.deck” as its file extension.</p>
+				<p>If you export a single deck alone, then the deck data file is named after that deck’s name. If you export multiple decks together, then the file is named “My Decks.” In either case, the file has “.deck” as its file extension.</p>
 			</li>
 			<li>
 				<h5>Delete</h5>
-				<p>You’re taken to the Delete Decks page, where you can select any of your decks to permanently remove from MTG Deck Builder. (If you’d later try to access the URL that belonged to the deck page of a deleted deck, then you’d be redirected to a “Page Not Found” error page instead.)</p>
+				<p>You’re taken to the Delete Decks page, where you can select any of your decks to permanently erase from MTG Deck Builder.</p>
+				<p>(After a deck has been deleted, if you’d try to access the URL that belonged to the deck page of that deck, then you’d be redirected to a “Page Not Found” error page instead.)</p>
 			</li>
 		</ul>
 	</section>
