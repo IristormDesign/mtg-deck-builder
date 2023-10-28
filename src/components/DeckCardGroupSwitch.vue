3ef076a9db36887ad2d20136749cc5cbf3875de3
Damian Schmidt <damian@iristormdesign.com>
2023-10-28 00:59:53 -0400

    Improved deck control panels

--- src/components/DeckCardGroupSwitch.vue
+++ src/components/DeckCardGroupSwitch.vue
@@ -1,28 +1,30 @@
 <template>
 	<section class="card-group-switch">
-		<h3>Switch Card Group:</h3>
-		<fieldset class="radio-fieldset">
-			<div>
-				<label for="radio-main-group">Main</label>
-				<input
-					id="radio-main-group"
-					type="radio"
-					checked
-					name="view-list"
-					@click="$store.commit('showSideboard', false)"
-					ref="checkRadioMainDeck"
-				/>
-			</div>
-			<div>
-				<label for="radio-sideboard-group">Sideboard</label>
-				<input
-					id="radio-sideboard-group"
-					type="radio"
-					name="view-list"
-					@click="$store.commit('showSideboard', true)"
-				/>
-			</div>
-		</fieldset>
+		<div>
+			<h3>Switch Card Group:</h3>
+			<fieldset class="radio-fieldset">
+				<div>
+					<label for="radio-main-group">Main</label>
+					<input
+						id="radio-main-group"
+						type="radio"
+						checked
+						name="view-list"
+						@click="$store.commit('showSideboard', false)"
+						ref="checkRadioMainDeck"
+					/>
+				</div>
+				<div>
+					<label for="radio-sideboard-group">Sideboard</label>
+					<input
+						id="radio-sideboard-group"
+						type="radio"
+						name="view-list"
+						@click="$store.commit('showSideboard', true)"
+					/>
+				</div>
+			</fieldset>
+		</div>
 	</section>
 </template>
 
