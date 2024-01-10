e83383cd1a13488ebc993f69943867b7355e3270
Damian Schmidt <damian@iristormdesign.com>
2024-01-10 00:13:13 -0500

    Improved 404 error pages

--- src/components/NotFoundContent.vue
+++ src/components/NotFoundContent.vue
@@ -11,3 +11,14 @@
 		<p v-if="this.$route.path.includes(`/deck/`)">Did you arrive on this error page expecting to see a deck page created by another user? If so, ask that user to export his or her deck as a deck data file and send it to you. For more info, <router-link to="/manual#m-storage-of-deck-data">see the user manual on storage of deck data</router-link>.</p>
 	</article>
 </template>
+
+<script>
+export default {
+	created () {
+		// Scroll the page to the top in case the 404 page is accessed from the back button and the page had been previously scrolled down.
+		this.$nextTick(() => {
+			window.scrollTo(0, 0)
+		})
+	}
+}
+</script>
