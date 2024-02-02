0b775285c72670eca38a8cedb442b589cc708ae0
Damian Schmidt <damian@iristormdesign.com>
2024-02-02 11:27:53 -0500

    Optimized code

--- src/mixins/sortingClusterGaps.js
+++ src/mixins/sortingClusterGaps.js
@@ -5,78 +5,83 @@ export default {
 		 * @param {string} sortBy
 		 */
 		addSortingClusterGaps (deck, sortBy) {
-			function isCreature (string) {
-				return /\bCreature\b/.test(string)
-			}
-			function isPlaneswalker (string) {
-				return /\bPlaneswalker\b/.test(string)
-			}
-			function isEnchantment (string) {
-				return /\bEnchantment\b/.test(string)
-			}
-			function isArtifact (string) {
-				return /\bArtifact\b/.test(string)
-			}
-			function isSorcery (string) {
-				return /\bSorcery\b/.test(string)
-			}
-			function isInstant (string) {
-				return /\bInstant\b/.test(string)
-			}
-			function isLand (string) {
-				return /\bLand\b/.test(string)
-			}
 			function findWhereToPutGaps (cards) {
 				for (let i = 0; i < cards.length; i++) {
-					const card = cards[i]
+					const thisCard = cards[i]
 					const nextCard = cards[(i + 1)]
 
-					card.gapAfter = false
+					thisCard.gapAfter = false
 
 					if (nextCard) {
-						if (sortBy === 'cmc') {
-							if (card.cmc < nextCard.cmc) {
-								card.gapAfter = true
+						if (sortBy === 'starred') {
+							if (thisCard.starred && !nextCard.starred) {
+								thisCard.gapAfter = true
+							}
+						} else if (sortBy === 'cmc') {
+							if (thisCard.cmc < nextCard.cmc) {
+								thisCard.gapAfter = true
 							}
 						} else if (sortBy === 'type') {
 							const regexFrontFaceType = (card) => {
 								// If the card is double-faced, get only the type of its front face (any string up to a slash character).
 								return card.type.match(/[^/]*/)[0]
 							}
-							const cardType = regexFrontFaceType(card)
+							const cardType = regexFrontFaceType(thisCard)
 							const nextCardType = regexFrontFaceType(nextCard)
+
+							function isCreature (string) {
+								return /\bCreature\b/.test(string)
+							}
+							function isPlaneswalker (string) {
+								return /\bPlaneswalker\b/.test(string)
+							}
+							function isEnchantment (string) {
+								return /\bEnchantment\b/.test(string)
+							}
+							function isArtifact (string) {
+								return /\bArtifact\b/.test(string)
+							}
+							function isSorcery (string) {
+								return /\bSorcery\b/.test(string)
+							}
+							function isInstant (string) {
+								return /\bInstant\b/.test(string)
+							}
+							function isLand (string) {
+								return /\bLand\b/.test(string)
+							}
 
 							if (isCreature(cardType)) {
 								if (!isCreature(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else if (isPlaneswalker(cardType)) {
 								if (!isPlaneswalker(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else if (isEnchantment(cardType)) {
 								if (!isEnchantment(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else if (isArtifact(cardType)) {
 								if (!isArtifact(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else if (isSorcery(cardType)) {
 								if (!isSorcery(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else if (isInstant(cardType)) {
 								if (!isInstant(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else if (isLand(cardType)) {
 								if (!isLand(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							} else { // A card type other than any of the standard card types
 								if (isCreature(nextCardType) || isPlaneswalker(nextCardType) || isEnchantment(nextCardType) || isArtifact(nextCardType) || isSorcery(nextCardType) || isInstant(nextCardType) || isLand(nextCardType)) {
-									card.gapAfter = true
+									thisCard.gapAfter = true
 								}
 							}
 						} else if (sortBy === 'subtype') {
@@ -86,8 +91,8 @@ export default {
 								return regexSubtype.test(card.type)
 							}
 
-							if (hasSubtype(card) && !hasSubtype(nextCard)) {
-								card.gapAfter = true
+							if (hasSubtype(thisCard) && !hasSubtype(nextCard)) {
+								thisCard.gapAfter = true
 							}
 						} else if (sortBy === 'supertype') {
 							const hasSupertype = (card) => {
@@ -96,24 +101,20 @@ export default {
 								return regexSupertype.test(card.type)
 							}
 
-							if (hasSupertype(card) && !hasSupertype(nextCard)) {
-								card.gapAfter = true
+							if (hasSupertype(thisCard) && !hasSupertype(nextCard)) {
+								thisCard.gapAfter = true
 							}
 						} else if (sortBy === 'pt-sum') {
-							if (card.power !== undefined && nextCard.power === undefined) {
-								card.gapAfter = true
+							if (thisCard.power !== undefined && nextCard.power === undefined) {
+								thisCard.gapAfter = true
 							}
 						} else if (sortBy === 'qty') {
-							if (card.qty > nextCard.qty) {
-								card.gapAfter = true
-							}
-						} else if (sortBy === 'starred') {
-							if (card.starred && !nextCard.starred) {
-								card.gapAfter = true
+							if (thisCard.qty > nextCard.qty) {
+								thisCard.gapAfter = true
 							}
 						}
 					} else { // The last card in the list (before any newly added cards)
-						card.gapAfter = true
+						thisCard.gapAfter = true
 					}
 				}
 			}
