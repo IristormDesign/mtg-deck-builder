d8724208987516e350063a3bb1bf371f07ec70ee
Damian Schmidt <damian@iristormdesign.com>
2024-02-03 00:07:22 -0500

    Moved card quantity elements to independent Vue component

---
+++ src/mixins/cardListFunctions.js
@@ -0,0 +1,28 @@
+export default {
+	computed: {
+		activeCardList () {
+			if (this.$store.state.showSideboard) {
+				return this.deck.sideboard
+			} else {
+				return this.deck
+			}
+		},
+		deckObject () {
+			return this.deck
+		}
+	},
+	methods: {
+		viewCard (card) {
+			const store = this.$store
+
+			if (store.state.showSideboard) {
+				this.deckObject.sideboard.viewedCard = card.name
+			} else {
+				this.deckObject.viewedCard = card.name
+			}
+
+			store.commit('decks', store.state.decks)
+			store.commit('showCard', true)
+		}
+	}
+}
