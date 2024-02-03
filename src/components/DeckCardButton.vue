7401f2167f5d51f55c10bcfdc3cacf8646d424d3
Damian Schmidt <damian@iristormdesign.com>
2024-02-03 00:19:21 -0500

    Moved card button elements to independent Vue component

---
+++ src/components/DeckCardButton.vue
@@ -0,0 +1,96 @@
+<template>
+	<button
+		@click="viewCard(card)"
+		:class="['card-button', colorButton(card)]"
+		:ref="card.name"
+	>
+		<div class="card-label-group">
+			<h4 class="name">{{ card.name }}</h4>
+			<div class="mana" v-html="styleManaSymbols(card)"></div>
+		</div>
+		<div class="card-label-group">
+			<div class="type">{{ card.type }}</div>
+			<div class="rarity" v-html="setRaritySymbol(card)"></div>
+		</div>
+	</button>
+</template>
+
+<script>
+import cardListFunctions from '@/mixins/cardListFunctions.js'
+import symbolsMarkup from '@/mixins/symbolsMarkup.js'
+
+export default {
+	mixins: [cardListFunctions, symbolsMarkup],
+	props: {
+		card: Object,
+		deck: Object
+	},
+	computed: {
+		focusCardButton () {
+			return this.$store.state.focusCardButton
+		}
+	},
+	watch: {
+		focusCardButton (name) {
+			this.$refs[name][0].focus() // Return browser focus to the card's button after the user has closed the card image pop-up (at narrow viewports).
+		}
+	},
+	methods: {
+		colorButton (card) {
+			const colors = card.colors
+			const w = colors.find(c => c === 'W')
+			const u = colors.find(c => c === 'U')
+			const b = colors.find(c => c === 'B')
+			const r = colors.find(c => c === 'R')
+			const g = colors.find(c => c === 'G')
+			const land = /\bLand\b/.test(card.type)
+
+			if (colors.length > 1) return 'multicolor'
+			else if (w) return 'white'
+			else if (u) return 'blue'
+			else if (b) return 'black'
+			else if (r) return 'red'
+			else if (g) return 'green'
+			else if (land) return 'land'
+		},
+		styleManaSymbols (card) {
+			const symbol = this.manaSymbol
+
+			return card.mana
+				.replaceAll(/{W}/g, symbol.w)
+				.replaceAll(/{U}/g, symbol.u)
+				.replaceAll(/{B}/g, symbol.b)
+				.replaceAll(/{R}/g, symbol.r)
+				.replaceAll(/{G}/g, symbol.g)
+				.replaceAll(
+					/{(\w+)}/g, // Find any other string having a pair of curly brackets with any other alphanumeric characters in between. This gets generic mana in both single and multiple digits and colorless mana ({C}).
+					'<span class="mana-symbol">$1</span>' // `$1` is a variable referring to the characters within the parentheses in the regex.
+				)
+				.replaceAll(
+					/{(\w+\/\w+)}/g, // Find any hybrid mana symbols (mana symbols containing a slash), such as `{G/W}`.
+					'<span class="mana-symbol hybrid" title="Hybrid mana"><div>$1</div></span>'
+				)
+				.replaceAll(
+					/{\w+\/\w+\/.*}/g, // Find hybrid mana symbols having 2 or more slashes, such as `{G/U/P}`. (These symbols are very rare but they do exist.)
+					'<span class="mana-symbol long-hybrid" title="Hybrid mana"><div>…</div></span>'
+				)
+		},
+		setRaritySymbol (card) {
+			const symbol = this.raritySymbol
+
+			switch (card.rarity) {
+				case 'common':
+					return symbol.c
+				case 'uncommon':
+					return symbol.u
+				case 'rare':
+					return symbol.r
+				case 'mythic':
+					return symbol.m
+				default:
+					return symbol.s
+			}
+		}
+	}
+}
+</script>
