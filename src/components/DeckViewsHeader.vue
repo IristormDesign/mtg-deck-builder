13263c4aac0247802abb8d5a89b2e17a12ceec80
Damian Schmidt <damian@iristormdesign.com>
2024-02-17 21:34:06 -0500

    Improved CSS of deck page header

--- src/components/DeckViewsHeader.vue
+++ src/components/DeckViewsHeader.vue
@@ -1,30 +1,27 @@
 <template>
-	<section class="deck-views content-box">
+	<nav class="deck-views deck-info">
 		<h3>{{ deckViewHeading }}</h3>
-
-		<nav>
-			<ul>
-				<li>
-					<router-link
-						:to="{ name: 'deckEditor' }"
-						:class="($route.name === 'deckEditor') ? 'current-page' : null"
-					>Deck Editor</router-link>
-				</li>
-				<li>
-					<router-link
-						:class="($route.name === 'moreStats') ? 'current-page' : null"
-						:to="{ name: 'moreStats' }"
-					>More Statistics</router-link>
-				</li>
-				<li>
-					<router-link
-						:class="($route.name === 'drawSimulator') ? 'current-page' : null"
-						:to="{ name: 'drawSimulator' }"
-					>Draw Simulator</router-link>
-				</li>
-			</ul>
-		</nav>
-	</section>
+		<ul>
+			<li>
+				<router-link
+					:to="{ name: 'deckEditor' }"
+					:class="($route.name === 'deckEditor') ? 'current-page' : null"
+				>Deck Editor</router-link>
+			</li>
+			<li>
+				<router-link
+					:class="($route.name === 'moreStats') ? 'current-page' : null"
+					:to="{ name: 'moreStats' }"
+				>More Statistics</router-link>
+			</li>
+			<li>
+				<router-link
+					:class="($route.name === 'drawSimulator') ? 'current-page' : null"
+					:to="{ name: 'drawSimulator' }"
+				>Draw Simulator</router-link>
+			</li>
+		</ul>
+	</nav>
 </template>
 
 <script>
