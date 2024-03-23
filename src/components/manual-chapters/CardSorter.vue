5d810f5de2b8c83ac8de9f7e2218b6228dfeb962
Damian Schmidt <damian@iristormdesign.com>
2024-03-23 15:07:41 -0400

    Minor edits to manual

--- src/components/manual-chapters/CardSorter.vue
+++ src/components/manual-chapters/CardSorter.vue
@@ -6,39 +6,40 @@
 				src="@/img/manual-card-sorter.jpg" width="350" height="185"
 				alt="Snippet of card sorter" title="Screenshot" loading="lazy" />
 		</p>
-		<p>The card sorter (the section labeled “Sort cards by” in the <a href="#m-deck-editor">Deck Editor</a>) lets you instantly sort all card names in the <a href="#m-card-list">card list</a> by a particular attribute you choose among various options.</p>
-		<p>Whenever you select an option from the card sorter’s drop-down menu, it sorts card names only for your currently <a href="#m-deck-menu">active deck</a> in both its main and sideboard <a href="#m-card-groups">groups</a>.</p>
+		<p>The card sorter (the section labeled “Sort cards by” in the <a href="#m-deck-editor">Deck Editor</a>) lets you instantly rearrange the order of all names in the <a href="#m-card-list">card list</a> in various ways.</p>
+		<p>The card sorter has a drop-down menu providing several card attributes as options. The card names listed in both the main and sideboard <a href="#m-card-groups">groups</a> of your currently <a href="#m-deck-menu">open deck</a> are sorted according to the attribute you select.</p>
 		<p>The sorting options and their effects include the following:</p>
 		<ul>
 			<li>
-				<p><strong>Starred</strong> – Sorts between <a href="#m-card-stars">starred</a> and non-starred. This sorting option is hidden when your deck has no card names with a star.</p>
+				<p><strong>Starred</strong> – Sorts between <a href="#m-card-stars">starred</a> and non-starred. This sorting option is hidden when no cards in your deck have a star.</p>
 			</li>
 			<li>
-				<p><strong>Name</strong> – Sorts alphabetically by name.</p>
+				<p><strong>Name</strong> – Sorts alphabetically by card name.</p>
 			</li>
 			<li>
-				<p><strong>Mana Color</strong> – Sorts between monocolored, multicolored, and colorless. Names of monocolored cards are sorted among each other in <i>Magic</i>’s conventional order of the mana colors: white, blue, black, red, green. Names of colorless cards are sorted among each other between spells and lands.</p>
+				<p><strong>Mana Color</strong> – Sorts between monocolored, multicolored, and colorless. Monocolored cards are sorted by <i>Magic</i>’s conventional order of the mana colors: white, blue, black, red, green. Colorless cards are sorted between spells and lands.</p>
 			</li>
 			<li>
 				<p><strong>Mana Value</strong> – Sorts from least to greatest by mana value (a.k.a. converted mana cost).</p>
 			</li>
 			<li>
-				<p><strong>Supertype</strong> – Sorts alphabetically by supertype, and moves names of cards without supertypes to the bottom. (Examples of supertypes are “basic” [on land cards] and “legendary.”)</p>
+				<p><strong>Supertype</strong> – Sorts alphabetically by supertype, and moves cards without supertypes to the bottom. (Examples of supertypes are “basic” [on land cards] and “legendary.”)</p>
 			</li>
 			<li>
-				<p><strong>Type</strong> – Sorts by card type in the following order: creatures, planeswalkers, battles, enchantments, artifacts, sorceries, instants, lands.</p>
+				<p><strong>Type</strong> – Sorts by card type in this order: creatures, planeswalkers, battles, enchantments, artifacts, sorceries, instants, lands.</p>
 			</li>
 			<li>
-				<p><strong>Subtype</strong> – Sorts alphabetically by subtype, and moves names of cards without subtypes to the bottom.</p>
+				<p><strong>Subtype</strong> – Sorts alphabetically by subtype, and moves cards without subtypes to the bottom.</p>
 			</li>
 			<li>
 				<p><strong>Rarity</strong> – Sorts from highest to lowest by level of rarity.</p>
 			</li>
 			<li>
-				<p><strong>P/T Sum</strong> – Sorts from greatest to least by “P/T sum,” which is a number equal to adding together the power and toughness (P/T) of a card. (For example, the P/T sum of a 3/2 creature is 5.) Among names of cards having the same P/T sums as each other, they’re sorted from greatest to least by power alone. Names of cards without the power and toughness characteristics are moved to the bottom.</p>
+				<p><strong>P/T Sum</strong> – Sorts from greatest to least by “P/T sum,” which is a number equal to adding together the power and toughness (P/T) of a card. (For example, the P/T sum of a 3/2 creature is 5.)</p>
+				<p>Among cards having the same P/T sums as each other, they’re sorted from greatest to least by power alone. Cards without the power and toughness characteristics are moved to the bottom.</p>
 			</li>
 			<li>
-				<p><strong>Quantity</strong> – Sorts from greatest to least by quantity.</p>
+				<p><strong>Quantity</strong> – Sorts from greatest to least by <a href="#m-card-quantities">quantity</a>.</p>
 			</li>
 		</ul>
 		<p>The results of sorting are cumulative, with the most recently selected sorting attribute given highest priority. For example, let’s say you first sort by rarity, and then by type. In effect, the card names are sorted mainly by type, and within each sorted cluster of types (the creatures, the sorceries, etc.), the card names are sorted by rarity.</p>
@@ -46,7 +47,7 @@
 		<p>The card sorter will reset its sorting option to “(None),” and any gaps between sorted clusters in the card list will be removed, under any of the following conditions:
 			<ul>
 				<li>Whenever you <a href="#m-card-adder">add a card name</a> to the list</li>
-				<li>Whenever you <a href="#m-card-quantities">adjust the quantity</a> of a card name while the sorter is set to “Quantity”</li>
+				<li>Whenever you adjust the quantity of a card name while the sorter is set to “Quantity”</li>
 				<li>Whenever you star a card name while the sorter is set to “Starred”</li>
 			</ul>
 		</p>
--- src/components/manual-chapters/ManualIntroduction.vue
+++ src/components/manual-chapters/ManualIntroduction.vue
@@ -4,8 +4,8 @@
 			<img class="intro-illustration" src="~@/img/urzas-tome.jpg" width="626" height="457" alt="Illustration of a large, detailed, open book" />
 			<figcaption>(Illustration: <i><a href="https://scryfall.com/card/dom/235/urzas-tome" target="_blank">Urza’s Tome</a></i> by Aaron Miller)</figcaption>
 		</figure>
-		<p>This document is the user manual for <router-link :to="{name: 'home'}">MTG Deck Builder by Iristorm Design</router-link>. Here you can learn the details about how this web app works.</p>
-		<p>The manual is divided into many chapters. You can skip directly to a chapter you want to read by clicking the relevant link in the table of contents or on certain other links throughout the manual.</p>
+		<p>This document is the user manual for <router-link :to="{name: 'home'}">MTG Deck Builder by Iristorm Design</router-link>. Here you can learn about how this web app works.</p>
+		<p>The manual is divided into many chapters. You can skip directly to a chapter you want to read by clicking the relevant link in the table of contents or any of the other chapter links throughout the manual.</p>
 		<p>The manual contains screenshots that show parts of MTG Deck Builder. Some of these screenshots have been taken from earlier versions of the app, which may look slightly different from the current version.</p>
 
 		<game-rules />
