712ac56a1cc641aaa3ca9bf2fcbbc2cf86c74b02
Damian Schmidt <damian@iristormdesign.com>
2024-04-11 11:49:57 -0400

    Optimized code

--- src/components/deck-page/MoreStatsKeywords.vue
+++ src/components/deck-page/MoreStatsKeywords.vue
@@ -6,17 +6,17 @@
 				<thead v-html="tableHeadCommon" />
 
 				<tbody
-					v-if="keywordNames.length === 0"
+					v-if="Object.keys(keywordCounts).length === 0"
 					v-html="tableBodyEmpty"
 				/>
 				<tbody v-else>
 					<tr
-						v-for="keyword in keywordNames"
-						:key="keyword"
+						v-for="(count, name) in keywordCounts"
+						:key="name"
 					>
-						<th>{{ keyword }}</th>
-						<td>{{ keywordCounts[keyword] }}</td>
-						<td>{{ calculatePercentage(keywordCounts[keyword]) }}</td>
+						<th>{{ name }}</th>
+						<td>{{ count }}</td>
+						<td>{{ calculatePercentage(count) }}</td>
 					</tr>
 				</tbody>
 			</table>
@@ -34,50 +34,24 @@ export default {
 	},
 	data () {
 		return {
-			allKeywords: [],
-			keywordNames: [],
 			keywordCounts: {}
 		}
 	},
-	mounted () {
-		this.findAllKeywords()
-		this.setUpKeywordCounts()
+	created () {
 		this.countKeywords()
 	},
 	methods: {
-		findAllKeywords () {
-			this.deck.cards.forEach(card => {
-				if (!card.keywords) return // This check is needed to potentially prevent an error caused by non-updated cards that lack the `keywords` attribute.
-
-				card.keywords.forEach(keyword => {
-					for (let i = 0; i < card.qty; i++) {
-						this.allKeywords.push(keyword)
-					}
-				})
-			})
-		},
-		setUpKeywordCounts () {
-			const kwNames = this.keywordNames
-
-			this.allKeywords.forEach(keyword => {
-				if (kwNames.indexOf(keyword) < 0) {
-					kwNames.push(keyword)
-				}
-			})
-
-			kwNames.sort()
-		},
 		countKeywords () {
-			this.keywordNames.forEach(name => {
-				this.keywordCounts[name] = 0
-			})
+			const counts = this.keywordCounts
 
-			this.allKeywords.forEach(keyword => {
-				for (const item in this.keywordCounts) {
-					if (keyword === item) {
-						this.keywordCounts[item]++
+			this.deck.cards.forEach(card => {
+				card.keywords.forEach(kw => {
+					if (!counts[kw]) {
+						counts[kw] = 0
 					}
-				}
+
+					counts[kw] += card.qty
+				})
 			})
 		}
 	}
