d0ead207afa1e4b82b2b8aec5d534e233f76ab4a
Damian Schmidt <damian@iristormdesign.com>
2024-08-25 19:10:14 -0400

    Changed file paths to not be relative

--- src/js/router.js
+++ src/js/router.js
@@ -13,12 +13,12 @@ const routes = [
 	{
 		name: 'manual',
 		path: '/manual',
-		component: () => import(/* webpackChunkName: "manual" */ '../views/UserManual.vue')
+		component: () => import(/* webpackChunkName: "manual" */ '@/views/UserManual.vue')
 	},
 	{
 		name: 'createDeck',
 		path: '/create-deck',
-		component: () => import(/* webpackChunkName: "create-deck" */ '../views/CreateDeck.vue')
+		component: () => import(/* webpackChunkName: "create-deck" */ '@/views/CreateDeck.vue')
 	},
 	{ // When a `deck` URL is missing a specified deck path, redirect to the home page.
 		path: '/deck',
@@ -31,57 +31,57 @@ const routes = [
 	},
 	{
 		path: '/deck/:deckPath',
-		component: () => import(/* webpackChunkName: "deck" */ '../views/DeckPage.vue'),
+		component: () => import(/* webpackChunkName: "deck" */ '@/views/DeckPage.vue'),
 		children: [
 			{
 				name: 'deckEditor',
 				path: '',
-				component: () => import(/* webpackChunkName: "deck-editor" */ '../views/DeckEditor.vue')
+				component: () => import(/* webpackChunkName: "deck-editor" */ '@/views/DeckEditor.vue')
 			},
 			{
 				name: 'deckAnalyzer',
 				path: 'deck-analyzer',
-				component: () => import(/* webpackChunkName: "deck-analyzer" */ '../views/DeckAnalyzer.vue')
+				component: () => import(/* webpackChunkName: "deck-analyzer" */ '@/views/DeckAnalyzer.vue')
 			},
 			{
 				name: 'drawSim',
 				path: 'draw-sim',
-				component: () => import(/* webpackChunkName: "draw-sim" */ '../views/DrawSim.vue')
+				component: () => import(/* webpackChunkName: "draw-sim" */ '@/views/DrawSim.vue')
 			}
 		]
 	},
 	{
 		name: 'exportDecks',
 		path: '/export-decks',
-		component: () => import(/* webpackChunkName: "export-decks" */ '../views/ExportDecks.vue'),
+		component: () => import(/* webpackChunkName: "export-decks" */ '@/views/ExportDecks.vue'),
 		props: true
 	},
 	{
 		name: 'deleteDecks',
 		path: '/delete-decks',
-		component: () => import(/* webpackChunkName: "delete-decks" */ '../views/DeleteDecks.vue'),
+		component: () => import(/* webpackChunkName: "delete-decks" */ '@/views/DeleteDecks.vue'),
 		props: true
 	},
 	{
 		name: 'contact',
 		path: '/contact',
-		component: () => import(/* webpackChunkName: "contact" */ '../views/ContactPage.vue')
+		component: () => import(/* webpackChunkName: "contact" */ '@/views/ContactPage.vue')
 	},
 	{
 		name: 'terms',
 		path: '/terms',
-		component: () => import(/* webpackChunkName: "terms" */ '../views/TermsOfService.vue')
+		component: () => import(/* webpackChunkName: "terms" */ '@/views/TermsOfService.vue')
 	},
 	{
 		name: 'privacy',
 		path: '/privacy',
-		component: () => import(/* webpackChunkName: "privacy" */ '../views/PrivacyPolicy.vue')
+		component: () => import(/* webpackChunkName: "privacy" */ '@/views/PrivacyPolicy.vue')
 	},
 	{
 		// This route should always be the last item in the `routes` array.
 		name: 'notFound',
 		path: '*',
-		component: () => import(/* webpackChunkName: "not-found" */ '../views/NotFound.vue')
+		component: () => import(/* webpackChunkName: "not-found" */ '@/views/NotFound.vue')
 	}
 ]
 
