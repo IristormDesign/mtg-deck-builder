3b46a65b38f1ee0b7ec91bf72764769b56f69fc3
Damian Schmidt <damian@iristormdesign.com>
2024-08-31 15:25:07 -0400

    Edited manual

--- src/components/manual-chapters/SideboardUses.vue
+++ src/components/manual-chapters/SideboardUses.vue
@@ -4,10 +4,10 @@
 		<p>The sideboard is a <a href="#m-card-groups">card group</a> containing cards that are related to your deck, but that aren’t in your deck’s main card group. In MTG Deck Builder, you could use the sideboard for any of these purposes:</p>
 		<ul>
 			<li>
-				<p>Temporarily swap certain cards in the main card group with ones in the sideboard. You’d typically do this before a rematch with an opponent whose deck you’ve realized has strengths and weaknesses against yours.</p>
+				<p>While you’re in the process of making a new deck, the sideboard group can hold cards you’re considering to put in the main group, but you haven’t firmly decided on yet. For this use, the sideboard can be helpful because the cards in it don’t affect your <a href="#m-basic-statistics">deck’s statistics</a>, except for Last Edit.</p>
 			</li>
 			<li>
-				<p>When you’re in the process of making a new deck, the sideboard group can hold cards you’re considering to put in the main group, but that you haven’t firmly decided on yet. For this use, the sideboard can be helpful because the cards in it don’t affect your <a href="#m-basic-statistics">deck’s statistics</a>, except for Last Edit.</p>
+				<p>After you finish making a deck, the sideboard group can keep extra cards as potential substitutes for some cards in the main group. You’d replace the cards to adapt your deck for a rematch with an opponent whose deck you’ve realized has certain strengths and weaknesses against yours.</p>
 			</li>
 			<li>
 				<p>Some cards have abilities that specially let you get cards from “outside the game.” The sideboard can contain the outside cards that you intend to play with your deck.</p>
