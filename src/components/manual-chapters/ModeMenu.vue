505e558c17605daa0f6d173dbffdb2400f654aff
Damian Schmidt <damian@iristormdesign.com>
2024-09-01 17:21:53 -0400

    Correction to manual

--- src/components/manual-chapters/ModeMenu.vue
+++ src/components/manual-chapters/ModeMenu.vue
@@ -1,6 +1,6 @@
 <template>
 	<section id="m-mode-menu">
 		<h4>Mode Menu</h4>
-		<p>The mode menu is a section of the <a href="#m-deck-page-header">deck page header</a> that displays the name of the currently set <a href="#m-deck-page-modes">deck page mode</a>, and has buttons for the three modes (<a href="#m-deck-editor">Deck Editor</a>, <a href="#m-deck-analyzer">Deck Analyzer</a>, and <a href="#m-draw-sim">Deck Simulator</a>). Click any of those buttons in the menu, or use a <a href="#m-kbsc-deck-page-modes">keyboard shortcut</a>, to set the <a href="#m-deck-pages">deck page</a> to your chosen mode.</p>
+		<p>The mode menu is a section of the <a href="#m-deck-page-header">deck page header</a> that displays the name of the currently set <a href="#m-deck-page-modes">deck page mode</a>, and has buttons for the three modes (<a href="#m-deck-editor">Deck Editor</a>, <a href="#m-deck-analyzer">Deck Analyzer</a>, and <a href="#m-draw-sim">Draw Simulator</a>). Click any of those buttons in the menu, or use a <a href="#m-kbsc-deck-page-modes">keyboard shortcut</a>, to set the <a href="#m-deck-pages">deck page</a> to your chosen mode.</p>
 	</section>
 </template>
