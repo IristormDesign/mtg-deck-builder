f9c328a65255b1499810baab2c667ab633d70d2b
Damian Schmidt <damian@iristormdesign.com>
2024-09-17 23:01:38 -0400

    Edited user manual text

--- src/components/manual-chapters/KbScDeckAnalyzer.vue
+++ src/components/manual-chapters/KbScDeckAnalyzer.vue
@@ -1,10 +1,10 @@
 <template>
 	<section id="m-kbsc-deck-analyzer">
 		<h4>Keyboard Shortcuts: Deck Analyzer Actions</h4>
-		<p>Only one <a href="#m-keyboard-shortcuts">keyboard shortcut</a> is specially available for the <a href="#m-deck-analyzer">Deck Analyzer</a>.</p>
+		<p>There’s only one <a href="#m-keyboard-shortcuts">keyboard shortcut</a> specially for the <a href="#m-deck-analyzer">Deck Analyzer</a>.</p>
 
 		<ul class="keyboard-shortcuts">
-			<li><kbd>Esc</kbd>: Stop the active statistics filter.</li>
+			<li><kbd>Esc</kbd>: Stop the active <a href="#m-statistics-filter">statistics filter</a>.</li>
 		</ul>
 	</section>
 </template>
