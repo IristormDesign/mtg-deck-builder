d49ece900a70cd943bb11bded10c30f2ce97b968
Damian Schmidt <damian@iristormdesign.com>
2024-09-24 16:38:10 -0400

    Manual: Edited text

--- src/components/manual-chapters/CardQuantities.vue
+++ src/components/manual-chapters/CardQuantities.vue
@@ -11,8 +11,8 @@
 		<p>To change a card name’s quantity, do any of the following methods:</p>
 		<ul>
 			<li>Click the quantity number field, then use your keyboard to type in a new number.</li>
-			<li>If you’re on a large-screen device, hover your mouse pointer over the quantity number to make a pair of buttons appear beside it. Of that pair of buttons, click the “+” (plus sign) button to increase the quantity, or the “&minus;” (minus sign) to decrease.</li>
-			<li>With the card list row highlighted via <a href="#m-keyboard-shortcuts">keyboard shortcuts</a>, press <kbd>E</kbd> to increase the quantity, or <kbd>D</kbd> to decrease. Or, while holding <kbd class="shift-key">Shift</kbd>, press <kbd>E</kbd> or <kbd>D</kbd> to adjust the quantity by 2 instead of 1.</li>
+			<li>If you’re on a large-screen device, hover your mouse pointer over the quantity number to make a pair of buttons appear beside it. Of that pair of buttons, click the “+” (plus sign) button to increase the quantity, or the “&minus;” (minus sign) to decrease it.</li>
+			<li>With the card list row highlighted via <a href="#m-keyboard-shortcuts">keyboard shortcuts</a>, press <kbd>E</kbd> to increase the quantity, or <kbd>D</kbd> to decrease it. Or, while holding <kbd class="shift-key">Shift</kbd>, press <kbd>E</kbd> or <kbd>D</kbd> to adjust the quantity by 2 instead of 1.</li>
 		</ul>
 		<p>To remove a card name from the card list, decrease its quantity to 0, or delete the quantity number itself. Before the name actually gets removed, the app asks you to confirm the removal. If you choose to cancel instead, then the name stays in your deck with its quantity reset to 1.</p>
 		<p>Conforming to <a href="#m-game-rules"><i>Magic</i>’s rules</a>, MTG Deck Builder prevents you from setting card names to a quantity greater than 4—except for the cards of the “basic land” type, and a few certain cards with abilities that specially raise their quantity limits.</p>
--- src/components/manual-chapters/StatisticsFilter.vue
+++ src/components/manual-chapters/StatisticsFilter.vue
@@ -1,7 +1,7 @@
 <template functional>
 	<section id="m-statistics-filter" key="statistics-filter">
 		<h4>Statistics Filter</h4>
-		<p>When you’re on the <a href="#m-deck-analyzer">Deck Analyzer</a>, there may be times when you want to examine data about a specific aspect of your deck instead of your deck as a whole. For those times, you can use a statistics filter, which narrows down the information revealed to you based on any card attribute you choose.</p>
+		<p>When you’re in the <a href="#m-deck-analyzer">Deck Analyzer</a>, there may be times when you want to examine data about a specific aspect of your deck rather than data about your whole deck in general. For those times, you can use a statistics filter, which narrows down the information revealed to you based on any card attribute you choose.</p>
 		<p>To turn on a filter in the Deck Analyzer, click the row of the <a href="#m-data-tables">data table</a> regarding the card attribute that you want the statistics to focus on. The rows that can activate filters are the ones with the attributes’ names colored orange instead of white.</p>
 		<p>The exception to turning on and off filters is in the “Power & Toughness” category’s greatest/median/least subsection, where you click a particular cell of a row instead of any cell within a row.</p>
 		<p>Whenever a filter is turned on, the following effects happen:</p>
@@ -9,10 +9,10 @@
 			<li>Any data about your deck’s cards that doesn’t match the selected card attribute is temporarily hidden. All data will return when the filter is turned off.</li>
 			<li>For the table row relating to the filtered attribute, it distinctly appears highlighted blue with a dashed blue outline.</li>
 			<li>At the top of the Deck Analyzer is the filter notification banner, which indicates the certain card attribute, if any, that’s currently being filtered for.</li>
-			<li>In each category, the numbers for the total counts and the percentages update to count only your deck’s remaining cards after filtering.</li>
+			<li>In each category, the numbers for the total counts and percentages update to count only your deck’s remaining cards after filtering.</li>
 		</ul>
-		<p>No more than one filter can be active at a time. If you turn on a new filter while another filter is already on, then the new filter replaces (rather than adds upon) the previous filter.</p>
-		<p>To turn off a filter, you can do any of the following actions:</p>
+		<p>No more than one filter can be active at a time. If you turn on a filter while another filter is already on, then the new filter replaces (rather than adds upon) the previous filter.</p>
+		<p>To turn <em>off</em> a filter, and thus restore all card data about your deck, you can do any of the following actions:</p>
 		<ul>
 			<li>From the filter notification banner, click the “Stop Filtering” button.</li>
 			<li>Click the table row of the currently filtered card attribute (highlighted blue).</li>
