8fab5ad6b8e66171472d2d6613bf954884a11ae3
Damian Schmidt <damian@iristormdesign.com>
2024-10-12 15:23:09 -0400

    Code improvements and optimizations

--- src/views/DeckAsText.vue
+++ src/views/DeckAsText.vue
@@ -1,51 +1,53 @@
 <template>
-	<article class="deck-as-text content-box">
+	<article
+		class="deck-as-text content-box"
+		:class="!mainNotEmpty && !sideboardNotEmpty ? 'no-cards' : null"
+	>
 		<h3>Deck as Text</h3>
 
-		<div class="button-container">
-			<a
-				v-show="deck.cards.length > 0 || sideboardNotEmpty"
-				:download="`${deck.name}.txt`"
-				:href="`data:text/plain;charset=utf-8,${combinedMainAndSideboardLists}`"
-			>Save Deck as Text File</a>
-		</div>
+		<template v-if="mainNotEmpty || sideboardNotEmpty">
+			<div class="button-container">
+				<a
+					v-show="mainNotEmpty || sideboardNotEmpty"
+					:download="`${deck.name}.txt`"
+					:href="`data:text/plain;charset=utf-8,${combinedMainAndSideboardLists}`"
+				>Save Deck as Text File</a>
+			</div>
 
-		<div
-			class="lists"
-			:class="sideboardNotEmpty ? 'two-columns' : 'one-column'"
-		>
-			<section v-if="deck.cards.length > 0">
-				<header>
-					<h4>Main List</h4>
-					<button @click="copyList('mainList', 'main')">Copy List</button>
-				</header>
-				<textarea
-					v-text="listCards()"
-					ref="mainList"
-					readonly
-					:rows="textareaHeightMain"
-				></textarea>
-			</section>
-			<section v-else>
-				<header>
-					<h4>Main List</h4>
-				</header>
-				<p class="no-cards">(No cards)</p>
-			</section>
+			<div
+				class="lists"
+				:class="mainNotEmpty && sideboardNotEmpty ? 'two-columns' : null"
+			>
+				<section v-if="mainNotEmpty">
+					<header>
+						<h4>Main List</h4>
+						<button @click="copyList('mainList', 'main')">Copy List</button>
+					</header>
+					<textarea
+						v-text="listCards()"
+						ref="mainList"
+						readonly
+						:rows="textareaHeightMain"
+					></textarea>
+				</section>
+				<section v-if="sideboardNotEmpty">
+					<header>
+						<h4>Sideboard List</h4>
+						<button @click="copyList('sideboardList', 'sideboard')">Copy List</button>
+					</header>
+					<textarea
+						v-text="listCards(true)"
+						ref="sideboardList"
+						:rows="textareaHeightSideboard"
+						readonly
+					></textarea>
+				</section>
+			</div>
+		</template>
 
-			<section v-if="sideboardNotEmpty">
-				<header>
-					<h4>Sideboard List</h4>
-					<button @click="copyList('sideboardList', 'sideboard')">Copy List</button>
-				</header>
-				<textarea
-					v-text="listCards(true)"
-					ref="sideboardList"
-					:rows="textareaHeightSideboard"
-					readonly
-				></textarea>
-			</section>
-		</div>
+		<template v-else>
+			<p>This deck currently has no cards, so there’s nothing to show here. Add cards to the deck using the <router-link :to="{name: 'deckEditor'}">Deck Editor</router-link>.</p>
+		</template>
 	</article>
 </template>
 
@@ -55,6 +57,9 @@ import getActiveDeck from '@/mixins/getActiveDeck.js'
 export default {
 	mixins: [getActiveDeck],
 	computed: {
+		mainNotEmpty () {
+			return this.deck.cards.length > 0
+		},
 		sideboardNotEmpty () {
 			return this.deck.sideboard.cards.length > 0
 		},
@@ -67,16 +72,20 @@ export default {
 		combinedMainAndSideboardLists () {
 			const title = this.deck.name.toUpperCase() + '\n\n'
 
-			if (this.sideboardNotEmpty) {
-				return (
-					title +
-					'Main List:\n\n' +
-					this.listCards() +
-					'\n\nSideboard List:\n\n' +
-					this.listCards(true)
-				)
+			if (this.mainNotEmpty) {
+				if (this.sideboardNotEmpty) {
+					return (
+						title +
+						'Main List:\n\n' +
+						this.listCards() +
+						'\n\nSideboard List:\n\n' +
+						this.listCards(true)
+					)
+				} else {
+					return title + this.listCards()
+				}
 			} else {
-				return title + this.listCards()
+				return title + this.listCards(true)
 			}
 		}
 	},
