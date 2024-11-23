d46036890c862e6b6a7b6fba8b598bebf273777b
Damian Schmidt <damian@iristormdesign.com>
2024-11-23 13:11:05 -0500

    Edited text

--- src/components/manual-chapters/CardImage.vue
+++ src/components/manual-chapters/CardImage.vue
@@ -13,7 +13,7 @@
 		<p>Whenever you <a href="#card-adder">add a card name</a> to the card list, or whenever you navigate up or down the list’s rows by <a href="#keyboard-shortcuts">pressing the <kbd>W</kbd> or <kbd>S</kbd> keys</a>, the relevant image is automatically shown.</p>
 		<p>To look at the card image up closer, hover your mouse pointer over it, or press <kbd>Q</kbd>.</p>
 		<p>If you’re using MTG Deck Builder on a mobile device instead of a computer with a larger screen, then the card image is normally invisible. You can still look at any card’s image by tapping the card’s bar in the card list, which makes the image appear hovering over the page. Then, to hide the image, tap the “X” button by the page’s top-right corner.</p>
-		<p>Click/tap the image, or press <kbd class="shift-key">Shift</kbd>+<kbd>Q</kbd>, to see that card’s page on <a href="https://scryfall.com/" target="_blank">Scryfall</a>, which opens in a new browser tab. Scryfall is a search engine and database for <i>Magic</i> cards (and it’s also from where MTG Deck Builder <a href="#app-dev">gets card data</a>).</p>
+		<p>Click/tap the image, or press <span class="no-text-break"><kbd class="shift-key">Shift</kbd>+<kbd>Q</kbd></span>, to see that card’s page on <a href="https://scryfall.com/" target="_blank">Scryfall</a>, which opens in a new browser tab. Scryfall is a search engine and database for <i>Magic</i> cards (and it’s also from where MTG Deck Builder <a href="#app-dev">gets card data</a>).</p>
 		<p>Whenever the displayed image is of a double-faced card, a button labeled “Turn Over” appears below the image. Click that button, or press <kbd>Z</kbd>, to see the card’s back face.</p>
 	</section>
 </template>
