8d7592451e08589f601ab9347c0f422fed9630a8
Damian Schmidt <damian@iristormdesign.com>
2024-11-26 23:56:26 -0500

    Edited text

--- src/components/manual-chapters/DeckMenu.vue
+++ src/components/manual-chapters/DeckMenu.vue
@@ -5,7 +5,7 @@
 		<p>Beside each deck’s name are the symbols of the mana colors required by any of the deck’s spells, ordered from the most frequent color to the least.</p>
 		<p>If you’re using MTG Deck Builder on a mobile device, the deck menu appears as a section within the whole menu of the app header instead of as a separate menu.</p>
 		<p>Each deck listed in the deck menu is a link that takes you to the respective <a href="#deck-pages">deck page</a>.</p>
-		<p>When you visit MTG Deck Builder for the first time ever, it provides you two sample decks: <i>Dimir Espionage</i> and <i>Primal Genesis (Commander)</i>. You may modify these decks however you like, or <a href="#deck-actions">delete</a> them.</p>
+		<p>When you visit MTG Deck Builder for the first time ever, it provides you two sample decks: <i>Dimir Espionage</i> and <i>Primal Genesis (Commander)</i>. You may modify these decks however you like, or you may <a href="#deck-actions">delete</a> them.</p>
 		<p>If all decks in the app have been deleted, the Open Deck link in the app header becomes temporarily disabled until you <a href="#create-deck">create a deck</a>.</p>
 	</section>
 </template>
