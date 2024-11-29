722930fba01b87ba21f231093eee61674da59cb0
Damian Schmidt <damian@iristormdesign.com>
2024-11-29 12:54:26 -0500

    Edited text

--- src/components/manual-chapters/DeckActions.vue
+++ src/components/manual-chapters/DeckActions.vue
@@ -6,6 +6,18 @@
 		<section id="render-as-text-action">
 			<h5>Render as Text</h5>
 			<p>In the <a href="#deck-actions">Deck Actions</a> menu, select “Render as text” to go to the “Deck in Plain Text” page. From there, you can view the cards in your deck’s <a href="#card-groups">main and sideboard groups</a> as simple lists in the form of plain text. These plain text card lists have one card name per line, with each name preceded by its <a href="#card-quantities">quantity</a>.</p>
+			<p>Why might you want to see your deck in the form of plain text?</p>
+			<ul>
+				<li>
+					<p><strong>Shop online faster for your deck’s cards.</strong> On some online stores that sell <i>Magic</i> card singles, they provide a way to search for a whole list of cards at once. Stores may call this list-searching method “mass entry,” “deck builder,” etc. You can copy your plain-text deck lists from this app, then paste it into an online store’s mass entry search.</p>
+				</li>
+				<li>
+					<p><strong>Print out your deck lists.</strong> A plain text document is better suited for printing on paper than the web page of the <a href="#deck-editor">Deck Editor</a>.</p>
+				</li>
+				<li>
+					<p><strong>Share over the Internet.</strong> Plain text card lists are a human-readable format that doesn’t require other people to use MTG Deck Builder, unlike <a href="#deck-data-files">deck data files</a>. (However, a deck data file can <a href="#export-action">export</a> and import multiple decks together, and includes extra data about decks that a plain text list lacks.)</p>
+				</li>
+			</ul>
 			<p>If you want to take these plain text card lists outside MTG Deck Builder, there are easy ways to do that:</p>
 			<ul>
 				<li>
@@ -15,19 +27,7 @@
 					<p>For either the main or the sideboard group of your deck, you can click the Copy button beside the list to instantly select and copy the whole text list to the clipboard of your computer or mobile device. (Then you can paste it elsewhere.)</p>
 				</li>
 			</ul>
-			<p>Why might you want to have your deck in the form of plain text?</p>
-			<ul>
-				<li>
-					<p><strong>Shop online faster for your deck’s cards.</strong> On some online stores that sell <i>Magic</i> card singles, they provide a way to search for a whole list of cards at once. Stores may call this list-searching method “mass entry,” “deck builder,” etc. You can copy your plain-text deck lists from this app, then paste it into a store’s mass entry search.</p>
-				</li>
-				<li>
-					<p><strong>Print out your deck lists.</strong> A plain text document is better suited for printing on paper than the web page of the <a href="#deck-editor">Deck Editor</a>.</p>
-				</li>
-				<li>
-					<p><strong>Share over the Internet.</strong> Plain text card lists are a human-readable format that doesn’t require other people to use MTG Deck Builder to read the card names, unlike <a href="#deck-data-files">deck data files</a>. (However, a deck data file can <a href="#export-action">export</a> and import multiple decks together, and includes extra deck data that a plain text list lacks.)</p>
-				</li>
-			</ul>
-			<p>The order of card names in plain text card lists matches that of the Deck Editor’s <a href="#card-list">card lists</a>. If you care about the order, use the Deck Editor’s <a href="#card-sorter">card sorter</a> to arrange the names in the way you prefer before converting your deck to text.</p>
+			<p>The order of card names in the plain text card lists matches that of the Deck Editor’s <a href="#card-list">card lists</a>. If you care about the order, use the Deck Editor’s <a href="#card-sorter">card sorter</a> to rearrange the names in the way you prefer before you render your deck as text.</p>
 		</section>
 
 		<section id="duplicate-action">
