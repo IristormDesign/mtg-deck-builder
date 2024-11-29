a04a967eb10906019141e487a4432ca2ea48ce54
Damian Schmidt <damian@iristormdesign.com>
2024-11-29 12:37:42 -0500

    Edited text

--- src/components/manual-chapters/ManualIntroduction.vue
+++ src/components/manual-chapters/ManualIntroduction.vue
@@ -6,7 +6,7 @@
 		</figure>
 		<p>This document is the user manual for <router-link :to="{name: 'home'}">MTG Deck Builder by Iristorm Design</router-link> (or just “MTG Deck Builder” for short). It should help you understand anything you want to know about how this web app works.</p>
 		<p>The manual is divided into many chapters. You can skip directly to a chapter you want to read by clicking the relevant link in the table of contents, or by clicking any chapter links throughout the manual’s text.</p>
-		<p>You could also use your web browser’s “Find” tool (press <span class="no-text-break"><kbd class="ctrl-key">Ctrl</kbd>+<kbd>F</kbd></span> or <span class="no-text-break"><kbd class="ctrl-key">⌘</kbd>+<kbd>F</kbd></span>) to instantly scroll the page to an instance of a word or phrase you specify.</p>
+		<p>You could also use your web browser’s “Find” tool (press <span class="no-text-break"><kbd class="ctrl-key">Ctrl</kbd>+<kbd>F</kbd></span> or <span class="no-text-break"><kbd class="ctrl-key">⌘</kbd>+<kbd>F</kbd>)</span> to instantly scroll the page to an instance of a word or phrase you specify.</p>
 		<p>The manual displays screenshots of parts of MTG Deck Builder. Note that some of these screenshots were taken from earlier versions of the app, so they may look slightly different from the current version.</p>
 
 		<game-rules />
