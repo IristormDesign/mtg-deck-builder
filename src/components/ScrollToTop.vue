4f5905f82ab59b60913dac4447be91936a963f1d
Damian Schmidt <damian@iristormdesign.com>
2024-12-12 00:21:38 -0500

    Added more icons

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -5,7 +5,7 @@
 			class="scroll-to-top"
 		>
 			<button @click="scrollToTop()">
-				<span>▲</span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M434.5-151.87v-481.98L215.76-415.11 151.87-480 480-808.13 808.13-480l-63.89 64.89L525.5-633.85v481.98h-91Z"/></svg>
 				<span class="text-label"> Scroll to Top</span>
 			</button>
 		</div>
--- src/components/deck-page/AnalyzerFilterNotice.vue
+++ src/components/deck-page/AnalyzerFilterNotice.vue
@@ -4,13 +4,19 @@
 		class="filter-notice active"
 	>
 		<div>
-			<h4>Filter: On</h4>
+			<header>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M444-169q-16 0-27.5-11T405-207v-235L179-730q-14.5-19-4.25-40T210-791h540q25 0 35.25 21T781-730L555-442v235q0 16-11.5 27T516-169h-72Zm36-299 195-248H285l195 248Zm0 0Z"/></svg>
+				<h4>Filter: On</h4>
+			</header>
 			<p v-show="activeFilterDescription">The displayed statistics involve only <strong>{{ activeFilterDescription }}</strong>.</p>
 			<div class="button-container">
 				<button
 					@click="stopFiltering()"
 					title="(Esc)"
-				>Stop Filtering</button>
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-96q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Zm0-72q55 0 104-18t89-50L236-673q-32 40-50 89t-18 104q0 130 91 221t221 91Zm244-119q32-40 50-89t18-104q0-130-91-221t-221-91q-55 0-104 18t-89 50l437 437Z"/></svg>
+					Stop Filtering
+				</button>
 			</div>
 		</div>
 	</aside>
@@ -19,13 +25,19 @@
 		class="filter-notice"
 	>
 		<div>
-			<h4>Filter: Off</h4>
+			<header>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M585.5-481 532-534.5 675-716H350.5l-75-75H750q24 0 34.75 21T781-730L585.5-481Zm201 413.5L555-299v91q0 16-11.5 27.5T516-169h-72q-16 0-27.5-11.5T405-208v-241L67-787l53-53.5 720 720-53.5 53ZM532-534.5Z"/></svg>
+				<h4>Filter: Off</h4>
+			</header>
 			<p>The displayed statistics involve your <strong>whole deck</strong>.</p>
 			<div class="button-container">
 				<button
 					disabled
 					title="No statistics filter is in use right now."
-				>Stop Filtering</button>
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-96q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Zm0-72q55 0 104-18t89-50L236-673q-32 40-50 89t-18 104q0 130 91 221t221 91Zm244-119q32-40 50-89t18-104q0-130-91-221t-221-91q-55 0-104 18t-89 50l437 437Z"/></svg>
+					Stop Filtering
+				</button>
 			</div>
 		</div>
 	</aside>
