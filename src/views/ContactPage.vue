e2bb6c522766852cfd49e6a86d0bdd5fb123145f
Damian Schmidt <damian@iristormdesign.com>
2024-12-16 14:08:53 -0500

    Edited text

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,7 +1,11 @@
 <template functional>
 	<article class="contact-page content-box">
 		<h2>Contact</h2>
-		<p>If you have questions or comments for the designer and developer of MTG Deck Builder, email Damian Schmidt at <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>.</p>
-		<p>You can view this app’s <a href="https://github.com/IristormDesign/mtg-deck-builder" target="_blank">source code on GitHub</a>.</p>
+
+		<h3>Email</h3>
+		<p>If you have questions or comments for the designer and developer of MTG Deck Builder, email Damian Schmidt at <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>. Bug reports are appreciated.</p>
+
+		<h3>GitHub</h3>
+		<p>Though MTG Deck Builder is copyrighted, you can view its <a href="https://github.com/IristormDesign/mtg-deck-builder" target="_blank">source code on GitHub</a>. You can contact the developer on there, too.</p>
 	</article>
 </template>
