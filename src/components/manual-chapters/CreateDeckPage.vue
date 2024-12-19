cd858d9c0e3400a7d82f373d689bf15875abdc2b
Damian Schmidt <damian@iristormdesign.com>
2024-12-19 13:14:17 -0500

    Edited text; updated some icons

--- src/components/manual-chapters/CreateDeckPage.vue
+++ src/components/manual-chapters/CreateDeckPage.vue
@@ -1,10 +1,19 @@
 <template functional>
 	<section id="create-deck" key="create-deck">
 		<h3>Create Deck Page</h3>
-		<p>The Create Deck page is where you can start on a new deck or import a pre-made deck. Either method adds the deck to the <a href="#deck-menu">deck menu</a> and <a href="#storage-of-deck-data">stores its data</a> in your web browser.</p>
-		<p>To start a new deck, give it a name, and then the app takes you to an empty <a href="#deck-pages">deck page</a> for that deck.</p>
-		<p>If you have a <a href="#deck-data-files">deck data file</a> (.deck) for MTG Deck Builder, you can import it through the Create Deck page, and the app instantly produces the decks from that file.</p>
-		<p>If you have a deck’s card list as plain text, such as one copied from the “<a href="#render-as-text-action">render as text</a>” deck action, you can have that deck displayed in MTG Deck Builder. To do this, start a new deck and paste the text list into the submission form of the <a href="#card-list-entry">Card List Entry</a> page. You can access that page by clicking the <a href="#add-card-list">Add Card List</a> button in the <a href="#list-editor">List Editor</a> deck page mode.</p>
+		<p>The Create Deck page is where you can begin new decks and import pre-made decks. Either method adds the deck to the <a href="#deck-menu">deck menu</a> and <a href="#storage-of-deck-data">stores its data</a> in your web browser.</p>
+
+		<h4>Start a New Deck</h4>
+		<p>To create a new deck, you give a name for it, and then the app takes you to an empty <a href="#deck-pages">deck page</a> assigned to that deck. You can build up your deck using the <a href="#card-adder">card adder</a>.</p>
+
+		<h4>Import Deck Data File</h4>
+		<p>What if you have a <a href="#deck-data-files">deck data file</a> (“.deck” file extension) and you want to see any of the decks contained in it? Then what you do is import the deck data file. When importing, the app promptly creates all the decks from that file’s data.</p>
+		<p>Note that this app can only import deck data files that had been exported from MTG Deck Builder by Iristorm Design. It cannot import files made from any other apps, even if they involve <i>Magic: The Gathering</i>.</p>
+
+		<h4>Create Decks from Plain Text Lists</h4>
+		<p>If you have a deck’s card list as plain text, such as one copied from the “<a href="#render-as-text-action">render as text</a>” deck action, then you can have that deck displayed in MTG Deck Builder. To do this, start a new deck, and then paste the text list into the submission form of the <a href="#card-list-entry">Card List Entry</a> page. You can access that page by clicking the <a href="#add-card-list">Add Card List</a> button in the <a href="#list-editor">List Editor</a> deck page mode.</p>
+
+		<h4>Deck Names</h4>
 		<p>Every deck you have in the app must have a unique name. When you’re starting a new deck, if you try to submit a name that another one of your decks already has, you’ll be asked to give a different name. Or, if you import a deck with the same name as an existing deck, then the imported deck’s name will automatically have a unique number within parentheses appended to it. (You could <a href="#deck-name">rename</a> it afterward.)</p>
 	</section>
 </template>
--- src/components/manual-chapters/DeckDataFiles.vue
+++ src/components/manual-chapters/DeckDataFiles.vue
@@ -4,12 +4,20 @@
 		<p>These are files that contain data for certain decks made with MTG Deck Builder by you or other users. A single deck data file can have data for multiple decks.</p>
 		<p>You might export or import a deck data file for any of the following reasons:</p>
 		<ul>
-			<li>Send the file to other users of MTG Deck Builder to let them see the decks you’ve made, or vice versa.</li>
-			<li>Keep the file as a backup copy of your decks, in case you somehow lose access to your original deck data in your computer or mobile device.</li>
-			<li>Move your decks from one web browser to another (if you’re switching your preferred browser, for example).</li>
-			<li>If you’d ever empty your web browser’s data cache, then all of your decks in MTG Deck Builder, whose data is stored in the cache, will be gone. You can preserve your decks by exporting them as a deck data file beforehand.</li>
+			<li>
+				<p>Send the file to other users of MTG Deck Builder to let them see the decks you’ve made, or vice versa.</p>
+			</li>
+			<li>
+				<p>Keep the file as a backup copy of your decks, in case you somehow lose access to your original deck data in your computer or mobile device.</p>
+			</li>
+			<li>
+				<p>Move your decks from one web browser to another (if you’re switching your preferred browser, for example).</p>
+			</li>
+			<li>
+				<p>If you’d ever empty your web browser’s data cache, then all of your decks in MTG Deck Builder, whose data is stored in the cache, will be gone. You can preserve your decks by exporting them as a deck data file beforehand.</p>
+			</li>
 		</ul>
-		<p>To export a deck (produce a deck data file), click the “<a href="#deck-actions">Deck Actions&hellip;</a>” button (in the <a href="#deck-page-header">deck page header</a>), then from the menu that appears, select Export.</p>
-		<p>To import a deck (view a deck contained in a deck data file), go to the <a href="#create-deck">Create Deck page</a>. From there, click the button in the Import Decks section, which lets you find and open a deck data file from the file system of your computer or mobile device.</p>
+		<p>To produce a deck data file, click the “<a href="#deck-actions">Deck Actions&hellip;</a>” button in the <a href="#deck-page-header">deck page header</a>, which makes a menu appear. From that menu, select Export.</p>
+		<p>To view a deck contained in a deck data file, go to the <a href="#create-deck">Create Deck page</a>. From there, click the button in the “Import Decks” section, which lets you find and open a deck data file from the file system of your computer or mobile device.</p>
 	</section>
 </template>
