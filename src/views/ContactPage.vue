085762dc9505a5c089b5aaaeb9ac451c9c4fcb23
Damian Schmidt <damian@iristormdesign.com>
2024-12-26 13:48:23 -0500

    Edited text

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -12,7 +12,7 @@
 			<p>If you have questions or comments for the designer and developer of MTG Deck Builder, email Damian Schmidt at <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>. Bug reports are appreciated.</p>
 
 			<h3>GitHub</h3>
-			<p>Though MTG Deck Builder is copyrighted, you can view its <a href="https://github.com/IristormDesign/mtg-deck-builder" target="_blank">source code on GitHub</a>. You can contact the developer on there, too.</p>
+			<p>Though MTG Deck Builder is copyrighted, you can view its <a href="https://github.com/IristormDesign/mtg-deck-builder" target="_blank">source code on GitHub</a>. You can contact the developer (@IristormDesign) on there, too.</p>
 		</div>
 	</article>
 </template>
