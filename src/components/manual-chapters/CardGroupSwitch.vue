38ff3b968f6947169bc5be616546d128cfb23e58
Damian Schmidt <damian@iristormdesign.com>
2025-01-02 18:34:30 -0500

    Edited text

--- src/components/manual-chapters/CardGroupSwitch.vue
+++ src/components/manual-chapters/CardGroupSwitch.vue
@@ -1,10 +1,9 @@
 <template>
 	<section id="card-groups">
 		<h4>Card Group Switch</h4>
-		<p>Each deck has two groups of cards called “main” and “<a href="#sideboard-uses">sideboard</a>.” These groups are independent of each other, and only one can be active at a time in the <a href="#list-editor">List Editor</a>.</p>
-		<p>The card group switch has a pair of buttons that lets you toggle between your deck’s main and sideboard groups. It appears in the section labeled “View card group” in the List Editor.</p>
-		<p>The <a href="#card-list">card list</a> and <a href="#card-adder">card adder</a> interact with the group that’s currently active.</p>
-		<p>To toggle the active card group via a <a href="#keyboard-shortcuts">keyboard shortcut</a>, press <kbd>R</kbd>.</p>
+		<p>Each deck has two independent groups of cards called “main” and “<a href="#sideboard-uses">sideboard</a>.” The <a href="#list-editor">List Editor</a> displays only one of these groups at a time.</p>
+		<p>The card group switch in the List Editor is a pair of buttons that appears in the section labeled “View card group,” and it lets you toggle between your deck’s main and sideboard groups for viewing. Alternatively, <a href="#keyboard-shortcuts">press the <kbd>R</kbd> key</a> to do that same action.</p>
+		<p>The <a href="#card-list">card list</a> and <a href="#card-adder">card adder</a> interact with whichever group you’re currently viewing.</p>
 		<p>Whenever you arrive on any deck page, the card group is automatically set to main at first. Whenever you switch to the sideboard, the <a href="#sideboard-header">sideboard header</a> appears.</p>
 
 		<sideboard-uses />
