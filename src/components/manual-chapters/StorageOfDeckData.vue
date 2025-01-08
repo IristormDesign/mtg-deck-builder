515fcb4b37f1f1ab2f5345b0132fd82de3116c9c
Damian Schmidt <damian@iristormdesign.com>
2025-01-08 15:42:25 -0500

    Edited text

--- src/components/manual-chapters/StorageOfDeckData.vue
+++ src/components/manual-chapters/StorageOfDeckData.vue
@@ -1,7 +1,8 @@
 <template>
 	<section id="storage-of-deck-data">
 		<h3>Storage of Deck Data</h3>
-		<p>The data for your customized decks is stored locally in the data cache of the web browser you’re using, not remotely in a database over the Internet. Basically, this means there’s no need to log onto a user account to save your decks in this app. It also means that other users of MTG Deck Builder on their own web browsers won’t be able to see your decks, or you theirs, except by sharing <a href="#deck-data-files">deck data files</a>.</p>
+		<p>In MTG Deck Builder, the data for your decks is stored in your web browser’s local data cache. The data isn’t stored over the Internet.</p>
+		<p>Basically, this means you can create and save your own decks in this app without needing to log onto a personal user account. But it also means that other users of MTG Deck Builder on their own browsers can’t see your decks, or you theirs, except by sharing <a href="#deck-data-files">deck data files</a>.</p>
 
 		<deck-data-files />
 	</section>
