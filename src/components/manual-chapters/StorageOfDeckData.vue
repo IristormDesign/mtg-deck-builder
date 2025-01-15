230634a2aef46ab565250cac14473206dbdc9291
Damian Schmidt <damian@iristormdesign.com>
2025-01-14 19:17:15 -0500

    Misc minor edits

--- src/components/manual-chapters/StorageOfDeckData.vue
+++ src/components/manual-chapters/StorageOfDeckData.vue
@@ -1,14 +1,14 @@
 <template functional>
 	<section id="storage-of-deck-data" key="storage-of-deck-data">
 		<h3>Storage of Deck Data</h3>
-		<p>In MTG Deck Builder, the data for your decks is stored only in your web browser’s local data cache. The data isn’t stored in a remote database through the Internet.</p>
-		<p>Basically, that means the following conditions:</p>
+		<p>In MTG Deck Builder, the data for your decks is stored only in your web browser’s local data cache. The data isn’t stored in a remote database over the Internet.</p>
+		<p>Basically, that means this app’s storage of deck data has the following conditions:</p>
 		<ul>
 			<li>
-				<p>Anyone can create and save decks in MTG Deck Builder right away, without ever needing to register for a user account.</p>
+				<p>Anyone can create and save decks in MTG Deck Builder right away, without even needing to register for a user account.</p>
 			</li>
 			<li>
-				<p>The custom decks you’ve made in MTG Deck Builder are available only in the particular web browser on the particular computer or phone on which you’ve made those decks. If you’d need to access your decks elsewhere, you can transfer their data from the source location by <a href="#archive-action">archiving your decks</a> beforehand, then <a href="#create-deck-page">reproducing the archived decks</a> in the other location.</p>
+				<p>The custom decks you’ve made in MTG Deck Builder are available only in the particular web browser on the particular computer or phone that you’ve been using this app on. If you’d need to access your pre-made decks elsewhere, you can transfer their data at the source location by <a href="#archive-action">archiving the decks</a> beforehand, then <a href="#create-deck-page">reproducing the archived decks</a> at the other location.</p>
 			</li>
 			<li>
 				<p>Similarly, other users of MTG Deck Builder on their own web browsers can’t view your decks, or you theirs—except by sharing <a href="#deck-archive-files">deck archive files</a>, or by the list-sharing methods via the <a href="#text-renderer">Text Renderer mode</a>.</p>
