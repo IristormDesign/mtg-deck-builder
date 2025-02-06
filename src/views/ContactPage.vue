b59011c7afb746814c6d029ab3ef3f4588c507ec
Damian Schmidt <damian@iristormdesign.com>
2025-02-05 20:33:01 -0500

    Improve layout and styling for Contact Page

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -2,13 +2,14 @@
 	<article class="contact-page content-box">
 		<h2>Contact</h2>
 
+		<p>The creator of MTG Deck Builder is Damian Schmidt of <a href="https://iristormdesign.com/" target="_blank">Iristorm Design</a>. You can contact him for questions or comments via any of the following media. Bug reports are appreciated.</p>
+
 		<figure>
 			<img class="card-illustration" src="~@/img/tidings.jpg" width="626" height="457" alt="An illustration of a man's hand pressing a wax seal onto a scroll by candlelight" />
 			<figcaption>Illustration from <i><a href="https://scryfall.com/card/c18/108/tidings" target="_blank">Tidings</a></i> by Pete Venters</figcaption>
 		</figure>
 
 		<div class="column">
-			<p>The creator of MTG Deck Builder is Damian Schmidt of <a href="https://iristormdesign.com/" target="_blank">Iristorm Design</a>. You can contact him for questions or comments via any of the following media. Bug reports are appreciated.</p>
 			<h3>Email</h3>
 			<p>Send an email to <a href="mailto:damian@iristormdesign.com">damian@iristormdesign.com</a>.</p>
 
