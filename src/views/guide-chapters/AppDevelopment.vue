3f4d7b4cb460397eda6caf45526b43f6d8d3b8ce
Damian Schmidt <damian@iristormdesign.com>
2025-03-11 10:28:22 -0400

    Edited text

--- src/views/guide-chapters/AppDevelopment.vue
+++ src/views/guide-chapters/AppDevelopment.vue
@@ -3,6 +3,6 @@
 		<h3>App Development</h3>
 		<p>MTG Deck Builder has been designed and developed solo by Damian Schmidt of <a href="https://iristormdesign.com/" target="_blank">Iristorm Design</a>. You can read an article about <a href="https://iristormdesign.com/portfolio/mtg-deck-builder/" target="_blank">how this app was made</a>.</p>
 		<p>Data and images of <i>Magic</i> cards in MTG Deck Builder are provided by <a href="https://scryfall.com/" target="_blank">Scryfall</a> through its public API. Iristorm Design is not affiliated with Scryfall.</p>
-		<p>You can <router-link :to="{name: 'contact'}">contact the creator</router-link> of MTG Deck Builder for questions or comments.</p>
+		<p>If you thought any information in this user guide was missing or unclear, you can directly <router-link to="/contact">contact the creator</router-link> of MTG Deck Builder for help.</p>
 	</section>
 </template>
