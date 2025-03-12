26f406e33b892328263b0da954b9bec4ef3a27b0
Damian Schmidt <damian@iristormdesign.com>
2025-03-11 23:40:19 -0400

    Optimized code

--- src/components/deck-page/StatDateEdited.vue
+++ src/components/deck-page/StatDateEdited.vue
@@ -13,11 +13,6 @@ export default {
 	props: {
 		deck: Object
 	},
-	data () {
-		return {
-			deckObject: this.deck
-		}
-	},
 	computed: {
 		dateEdited () {
 			const dateTime = new Date(this.deck.editDate)
@@ -27,18 +22,30 @@ export default {
 
 			function monthNameFromNumber () {
 				switch (dateTime.getMonth() + 1) {
-					case 1: return 'Jan.'
-					case 2: return 'Feb.'
-					case 3: return 'Mar.'
-					case 4: return 'Apr.'
-					case 5: return 'May'
-					case 6: return 'June'
-					case 7: return 'July'
-					case 8: return 'Aug.'
-					case 9: return 'Sep.'
-					case 10: return 'Oct.'
-					case 11: return 'Nov.'
-					case 12: return 'Dec.'
+					case 1:
+						return 'Jan.'
+					case 2:
+						return 'Feb.'
+					case 3:
+						return 'Mar.'
+					case 4:
+						return 'Apr.'
+					case 5:
+						return 'May'
+					case 6:
+						return 'June'
+					case 7:
+						return 'July'
+					case 8:
+						return 'Aug.'
+					case 9:
+						return 'Sep.'
+					case 10:
+						return 'Oct.'
+					case 11:
+						return 'Nov.'
+					case 12:
+						return 'Dec.'
 				}
 			}
 
