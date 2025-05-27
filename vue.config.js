b83e3759741fea637bb4cbbf4ee84563932b959d
Damian Schmidt <damian@iristormdesign.com>
2025-05-26 22:47:37 -0400

    Remove obsolete VUE_APP_VERSION assignment from vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
-process.env.VUE_APP_VERSION = require('./package.json').version
-
 module.exports = defineConfig({
 	transpileDependencies: true,
 	pages: {
