6a88019bb399a3174ae20f006e7d782d6a3c613a
Damian Schmidt <damian@iristormdesign.com>
2025-05-26 22:16:06 -0400

    Refactor HomePage component import to use dynamic import for improved code splitting

--- src/js/router.js
+++ src/js/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomePage from '@/views/HomePage.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,7 @@ const routes = [
 	{
 		name: 'home',
 		path: '/',
-		component: HomePage
+		component: () => import(/* webpackChunkName: "home" */ '@/views/HomePage.vue')
 	},
 	{
 		name: 'guide',
