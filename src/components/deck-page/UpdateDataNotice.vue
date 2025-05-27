8dbb075d853974008f91101807c273267eb3b5aa
Damian Schmidt <damian@iristormdesign.com>
2025-05-27 12:33:13 -0400

    CSS tweaks

--- src/components/deck-page/UpdateDataNotice.vue
+++ src/components/deck-page/UpdateDataNotice.vue
@@ -4,7 +4,7 @@
 		class="outdated-deck-data-notice"
 	>
 		<aside v-if="cardUpdateStatus === 1">
-			<h3>Card Data Update</h3>
+			<h3>Deck Data Update</h3>
 			<p>This deck has an outdated set of card data. Update it now to get enhanced features! See the May 23 <router-link to="/release-notes">release notes</router-link> for more info.</p>
 			<div class="button-container">
 				<button @click="userEngagedUpdate()">Update</button>
